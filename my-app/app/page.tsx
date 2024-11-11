338f3ca708a5cabfd3af2a507d72cbd7d60e8148
rmymartinet <remymartinet.contact@gmail.com>
2024-11-11 21:27:32 +0100

    Add GSAP animation and components for Features and GradiantSection

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,101 +1,155 @@
-import Image from "next/image";
+"use client";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+import { SiHellofresh } from "react-icons/si";
+import FeaturesContainer from "./_components/Features/FeaturesContainer";
+import ItemsFeatures from "./_components/Features/ItemsFeatures";
+import { default as GradiantSection } from "./_components/GradiantSection";
+
+gsap.registerPlugin(useGSAP);
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const chgRef = useRef<(HTMLDivElement | null)[]>([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const names = ["bonjour", "beh", "beh2", "bou"];
+
+  // useGSAP(() => {
+  //   const tl = gsap.timeline({ repeat: -1 });
+  //   chgRef.current.forEach((el) => {
+  //     tl.fromTo(
+  //       el,
+  //       {
+  //         rotate: 70,
+  //         x: 2000,
+  //         y: 2000,
+  //       },
+  //       {
+  //         rotate: 0,
+  //         x: 0,
+  //         y: 0,
+  //         duration: 1,
+  //         ease: "power2.inOut",
+  //       }
+  //     );
+  //     tl.to(el, {
+  //       delay: 2,
+  //       rotate: -70,
+  //       x: -2000,
+  //       y: 1000,
+  //       duration: 1,
+  //       ease: "power2.inOut",
+  //     });
+  //   });
+  // }, []);
+  return (
+    <>
+      <GradiantSection
+        gradiant="gradient1"
+        title="Programmes adaptées"
+        subtitle="Adapté à chaque niveau"
+        description="Débutant, intermédiaire, ou avancé ? Choisissez le programme qui correspond à votre niveau pour progresser efficacement, avec des techniques d’entraînement adaptées à chaque étape."
+      >
+        <FeaturesContainer>
+          <ItemsFeatures
+            logo={<SiHellofresh />}
+            title="Mode de Vie de Récupération"
+            paragraph="Découvre comment mieux récupérer pour progresser plus rapidement et éviter les blessures."
+          />
+          <ItemsFeatures
+            logo={<SiHellofresh />}
+            title="Vidéos de démonstration incluses"
+            paragraph="Accède à des démonstrations vidéos pour maîtriser chaque mouvement, à ton rythme et en toute sécurité."
+          />
+          <ItemsFeatures
+            logo={<SiHellofresh />}
+            title="Surcharge Progressive"
+            paragraph="Accède à des démonstrations vidéos pour maîtriser chaque mouvement, à ton rythme et en toute sécurité."
+          />
+          <ItemsFeatures
+            logo={<SiHellofresh />}
+            title="Suivi de l’Intensité"
+            paragraph="Accède à des démonstrations vidéos pour maîtriser chaque mouvement, à ton rythme et en toute sécurité."
+          />
+          <ItemsFeatures
+            logo={<SiHellofresh />}
+            title="Exercices Alternatifs"
+            paragraph="Accède à des démonstrations vidéos pour maîtriser chaque mouvement, à ton rythme et en toute sécurité."
+          />
+          <ItemsFeatures
+            logo={<SiHellofresh />}
+            title="Durée du Programme et Deloads"
+            paragraph="Accède à des démonstrations vidéos pour maîtriser chaque mouvement, à ton rythme et en toute sécurité."
+          />
+        </FeaturesContainer>
+      </GradiantSection>
+      <GradiantSection
+        gradiant="gradient2"
+        title="Coaching personalisé"
+        subtitle="Un suivi sur-mesure"
+        description="Trois mois pour démarrer ou se remettre en forme, jusqu’à neuf mois pour des transformations durables. Plus le coaching est long, plus les changements sont profonds et adaptés."
+      >
+        <FeaturesContainer>
+          <ItemsFeatures
+            logo={<SiHellofresh />}
+            title="Support WhatsApp 12h/24h"
+            paragraph="Transforme tes doutes en réponses immédiates ! Profite d’un accompagnement réactif pour maximiser tes résultats."
+          />
+          <ItemsFeatures
+            logo={<SiHellofresh />}
+            title="Bilan des progrès chaque semaine"
+            paragraph="Suis chaque étape de ta progression et reste motivé avec un bilan complet de tes performances chaque semaine."
+          />
+          <ItemsFeatures
+            logo={<SiHellofresh />}
+            title="Suivi de l’alimentation"
+            paragraph="Obtiens des conseils personnalisés pour optimiser ton alimentation et booster tes performances."
+          />
+          <ItemsFeatures
+            logo={<SiHellofresh />}
+            title="Applications de suivi"
+            paragraph="Obtiens des conseils personnalisés pour optimiser ton alimentation et booster tes performances."
+          />
+          <ItemsFeatures
+            logo={<SiHellofresh />}
+            title="Vidéos de démonstration incluses"
+            paragraph="Accède à des démonstrations vidéos pour maîtriser chaque mouvement, à ton rythme et en toute sécurité."
+          />
+        </FeaturesContainer>
+      </GradiantSection>
+
+      <div className="overflow-hidden h-screen  flex flex-col items-center justify-center p-6 relative bg-white">
+        <h1 className="absolute top-0 left-0 m-6 text-6xl">
+          Changer comme prénom
+        </h1>
+        <div className="flex items-center justify-center w-full h-full">
+          {names.map((test, index) => (
+            <div
+              key={index}
+              ref={(el) => {
+                chgRef.current[index] = el;
+              }}
+              className="absolute w-[40vw] h-[40vh] grid grid-cols-2 justify-items-center items-end "
+            >
+              {names[index]}
+              {/* <Image
+             className="absolute top-0 left-0 w-full h-full object-cover"
+             src="/test.png"
+             alt=""
+             width={500}
+             height={500}
+           /> */}
+              <div className="bg-red-400">
+                <span>0 mois</span>
+              </div>
+              <div className="bg-red-400">
+                <span>6 mois</span>
+              </div>
+            </div>
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   );
 }
