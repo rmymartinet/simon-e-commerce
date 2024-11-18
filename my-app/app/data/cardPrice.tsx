220ea7dc74841cbeebfba9b5e5c0e0de78dd3c7e
rmymartinet <remymartinet.contact@gmail.com>
2024-11-18 09:57:17 +0100

    Add data for card price

---
+++ my-app/app/data/cardPrice.tsx
@@ -0,0 +1,32 @@
+export const programIncludes = [
+  "Vidéos de démonstration incluses",
+  "Exercices Alternatifs",
+  "Mode de Vie de Récupération",
+  "Surcharge Progressive",
+  "Suivi de l’Intensité",
+  "Durée du Programme et Deloads",
+];
+
+export const coachingIncludes3Mounth = [
+  "Ajustements bi-mensuels",
+  "Planification de la récupération ",
+  "Support prioritaire 24/7 ",
+  "Séances de coaching en visio mensuelles",
+];
+
+export const coachingIncludes6Mounth = [
+  "Ajustements hebdomadaires ",
+  "Planification de la récupération avancée",
+  "Bilan de progression intermédiaire ",
+  "Séances de coaching en visio bimensuelles",
+  "Accès à des ressources exclusives",
+];
+
+export const coachingIncludes9Mounth = [
+  "Programme d’entraînement évolutif",
+  "Évaluation trimestrielle complète",
+  "Conseils nutrition avancés et réévaluations régulières ",
+  "Séances de coaching en visio (hebdomadaires au besoin)",
+  "Accès à des challenges mensuels exclusifs",
+  "Plan de maintien personnalisé en fin de programme",
+];
