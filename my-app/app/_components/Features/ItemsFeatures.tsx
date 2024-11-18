5f29ce599f821a1032f74bcad231d962da52a2e8
rmymartinet <remymartinet.contact@gmail.com>
2024-11-18 09:58:16 +0100

    Add animation to ItemsFeatures component

--- my-app/app/_components/Features/ItemsFeatures.tsx
+++ my-app/app/_components/Features/ItemsFeatures.tsx
@@ -1,3 +1,10 @@
+import { revealRotateEl } from "@/app/utils/Animation";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+
+gsap.registerPlugin(useGSAP);
+
 interface ItemsFeaturesProps {
   logo: React.ReactNode;
   title: string;
@@ -5,8 +12,14 @@ interface ItemsFeaturesProps {
 }
 
 const ItemsFeatures = ({ logo, title, paragraph }: ItemsFeaturesProps) => {
+  const containerRef = useRef(null);
+
+  useGSAP(() => {
+    revealRotateEl(containerRef);
+  }, []);
+
   return (
-    <div className="flex flex-col gap-4">
+    <div ref={containerRef} className="flex flex-col gap-4">
       <div>{logo}</div>
       <h2 className="text-2xl">{title}</h2>
       <p className="text-slate-400 text-pretty w-full lg:w-[24vw]">
