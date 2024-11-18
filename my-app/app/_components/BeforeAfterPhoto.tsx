2041b4392b1a8e64dc1fa16ea0d888523b6b72b8
rmymartinet <remymartinet.contact@gmail.com>
2024-11-18 09:58:36 +0100

    Add BeforeAfterPhoto component with image comparison functionality

--- my-app/app/_components/BeforeAfterPhoto.tsx
+++ my-app/app/_components/BeforeAfterPhoto.tsx
@@ -2,88 +2,88 @@
 
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import { useRef } from "react";
 
 gsap.registerPlugin(useGSAP);
 
 const BeforeAfterPhoto = () => {
   const chgRef = useRef<(HTMLDivElement | null)[]>([]);
-  const namesRef = useRef<HTMLHeadingElement>(null);
-  const names = ["Romain", "Rémy", "Théo", "ANouk"];
-  const [index, setIndex] = useState(0);
+  const names = ["Romain", "Rémy", "Théo", "Anouk"];
+  const imagesUrls = ["/images/beforeafter/1.jpg", "/images/beforeafter/2.jpg"];
+  const namesRef = useRef<(HTMLDivElement | null)[]>([]);
 
-  useEffect(() => {
+  useGSAP(() => {
     const tl = gsap.timeline({ repeat: -1 });
 
-    chgRef.current.forEach((el, index) => {
-      tl.fromTo(
-        el,
-        {
-          rotate: 70,
-          x: 2000,
-          y: 2000,
-        },
-        {
-          rotate: 0,
-          x: 0,
-          y: 0,
-          duration: 1,
-          ease: "power2.inOut",
-          onStart: () => {
-            setIndex(index);
-            gsap.fromTo(
-              namesRef.current,
-              { y: 100, opacity: 0 },
-              { y: 0, opacity: 1, duration: 1, ease: "power2.inOut" }
-            );
-          },
-        }
-      );
-      tl.to(el, {
-        delay: 2,
-        rotate: -70,
-        x: -2000,
-        y: 1000,
-        duration: 1,
-        ease: "power2.inOut",
-        onStart: () => {
-          gsap.to(namesRef.current, {
-            opacity: 0,
+    namesRef.current.forEach((el, i) => {
+      if (el) {
+        tl.fromTo(
+          el,
+          { opacity: 0, y: 100, rotate: 10 },
+          {
+            opacity: 1,
+            rotate: 0,
+            y: 0,
             duration: 1,
+            ease: "power2.out",
+          }
+        ),
+          tl.to(el, {
+            y: -100,
+            opacity: 0,
+            ease: "power2.out",
           });
-        },
-      });
+      }
     });
-  }, [names.length]);
+  }, []);
 
   return (
-    <div className="overflow-hidden h-screen flex flex-col items-center justify-center p-6 relative bg-white">
-      <div className="absolute top-0 left-0 m-6 inline-block gap-4">
+    <div className="flex flex-col items-center justify-center p-6 relative pb-40 ">
+      <div className="top-0 left-0 m-6 inline-flex gap-4 relative mb-40">
         <h1 className="text-6xl">Changer comme</h1>
-        <div className="overflow-hidden pb-4">
-          <h1 ref={namesRef} className="text-6xl">
-            {names[index]}
-          </h1>
+        <div className="overflow-hidden h-20 w-72 relative pb-4">
+          {names.map((name, idx) => (
+            <div
+              key={idx}
+              ref={(el) => {
+                namesRef.current[idx] = el;
+              }}
+              className="absolute text-6xl"
+            >
+              {name}
+            </div>
+          ))}
         </div>
       </div>
-      <div className="flex items-center justify-center w-full h-full">
-        {names.map((name, index) => (
-          <div
-            key={index}
-            ref={(el) => {
-              chgRef.current[index] = el;
-            }}
-            className="absolute w-[40vw] h-[40vh] grid grid-cols-2 justify-items-center items-end"
-          >
-            {name}
-            <div className="bg-red-400">
-              <span>0 mois</span>
+      <div className="flex gap-10  items-center justify-center w-full h-full ">
+        {names.map((name, index) => {
+          const imagePair = imagesUrls.slice(index * 2, index * 2 + 2);
+          return (
+            <div
+              key={index}
+              ref={(el) => {
+                chgRef.current[index] = el;
+              }}
+              className=" w-[18vw] grid grid-cols-2  rounded-xl overflow-hidden"
+            >
+              {imagesUrls.map((url, imageIndex) => (
+                <div className="relative" key={imageIndex}>
+                  <div className="absolute glassmorph text-white font-semibold w-full h-1/3 flex justify-center items-center">
+                    6 mois
+                  </div>
+                  <Image
+                    src={url}
+                    alt={`Image ${imageIndex}`}
+                    width={400}
+                    height={400}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+              ))}
             </div>
-            <div className="bg-red-400">
-              <span>6 mois</span>
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
