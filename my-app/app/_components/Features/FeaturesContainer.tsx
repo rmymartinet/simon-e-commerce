e05bf88aa56bdb336103a8787b72b5ff0f42575a
rmymartinet <remymartinet.contact@gmail.com>
2024-11-18 09:54:44 +0100

    Refactor FeaturesContainer component to include a dynamic title prop

--- my-app/app/_components/Features/FeaturesContainer.tsx
+++ my-app/app/_components/Features/FeaturesContainer.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 
 interface FeaturesContainerProps {
+  title: string;
   children: React.ReactNode;
 }
 
-const FeaturesContainer = ({ children }: FeaturesContainerProps) => {
+/**
+ * !TODO mt-40
+ */
+
+const FeaturesContainer = ({ children, title }: FeaturesContainerProps) => {
   return (
-    <div className="rounded-2xl flex justify-end gap-10 pb-[24vh]">
+    <div className="rounded-2xl flex justify-end gap-10 pb-[24vh] mt-40">
       <div className="h-full lg:w-[70vw] flex flex-col w-full gap-28">
-        <h1 className="text-4xl text-center md:text-start">
-          Fonctionnalités supplémentaires
-        </h1>
+        <div className="flex flex-col gap-4">
+          <h1 className="text-5xl text-center md:text-start">{title}</h1>
+          <p className="text-slate-400 text-lg">( sans suivi )</p>
+        </div>
         <div className="flex flex-col gap-32 md:grid md:grid-cols-2 md:grid-flow-row">
           {children}
         </div>
