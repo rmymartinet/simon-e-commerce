8dd69656c3874a225f2ce077e98057863b2bf755
rmymartinet <remymartinet.contact@gmail.com>
2024-11-18 09:55:37 +0100

    Add OverviewProgramContent component for displaying program content in the overview section

---
+++ my-app/app/_components/Overview/OverviewProgramContent.tsx
@@ -0,0 +1,18 @@
+import { OverviewSectionProps } from "@/types/types";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import ProgramContentDesktop from "./Desktop/ProgramContentDesktop";
+
+gsap.registerPlugin(useGSAP);
+
+const OverviewProgramContent = ({ gradient }: OverviewSectionProps) => {
+  return (
+    <div
+      className={`${gradient} rounded-2xl h-full w-full lg:col-start-2-end-4 relative overflow-hidden `}
+    >
+      <ProgramContentDesktop />
+    </div>
+  );
+};
+
+export default OverviewProgramContent;
