ba2a4e9b49fc389f993aded7b937d83f4fb5dbb7
rmymartinet <remymartinet.contact@gmail.com>
2024-11-18 09:53:56 +0100

    Add video background and buttons in Header component

--- my-app/app/_components/Header.tsx
+++ my-app/app/_components/Header.tsx
@@ -1,29 +1,52 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { IoIosArrowRoundForward } from "react-icons/io";
+
+gsap.registerPlugin(useGSAP);
+
 const Header = () => {
   return (
-    <div className="h-screen w-screen flex items-center justify-center p-8">
-      <div className="rounded-xl overflow-hidden div5 relative">
+    <div className="relative flex items-center justify-center p-8 min-h-screen">
+      <div className="absolute  overflow-hidden w-[100%] h-[100%]">
         <video
           autoPlay
           loop
           muted
-          src="/videos/sport.mp4"
-          className="w-full h-full object-cover"
+          src="/videos/text.mp4"
+          className="w-full h-full object-cover filter brightness-[0.9]"
         ></video>
-        <div className="absolute top-20 right-20  p-2 w-[30%]">
-          <p className="text-white text-2xl">
-            Accédez à des options flexibles et immédiates : PDF téléchargeables
-            pour progresser en autonomie ou coaching pour un accompagnement sur
-            mesure
-          </p>
-          <button className="bg-[#B06FF9] text-white p-4 rounded-xl">
-            Commencer
-          </button>
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full gap-2 flex flex-col items-center">
+          <div className="flex flex-col items-center"></div>
         </div>
-
-        <div className="absolute bottom-[10%] left-[3%] flex gap-4 w-[40%] ">
-          <h1 className="text-6xl text-emerald-100 rounded-xl p-2">
-            Programme Adapté Coaching Personnalisé
-          </h1>
+        <div className="flex flex-col p-4 text-black rounded-3xl items-center absolute bottom-10 left-1/2 -translate-x-1/2 border-2 bg-black border-slate-100 shadow-2xl">
+          <div className="gap-4 flex overflow-hidden">
+            <div className="bg-white p-2 rounded-xl ">
+              <div className="w-42  flex flex-col justify-between items-center p-2 gap-4 ">
+                <div>
+                  <h1 className="text-xl text-black">Programme</h1>
+                </div>
+                <button className="py-2 px-4 bg-black text-white rounded-xl flex justify-between items-center w-full">
+                  <p>Commencer</p>
+                  <div className="-rotate-45">
+                    <IoIosArrowRoundForward size={20} />
+                  </div>
+                </button>
+              </div>
+            </div>
+            <div className="p-2 rounded-xl bg-white">
+              <div className="w-42  flex flex-col justify-between items-center p-2 gap-4 ">
+                <div>
+                  <h1 className="text-xl text-black">Coaching</h1>
+                </div>
+                <button className="py-2 px-4 bg-black text-white rounded-xl flex justify-between items-center w-full">
+                  <p>Commencer</p>
+                  <div className="-rotate-45">
+                    <IoIosArrowRoundForward size={20} />
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
