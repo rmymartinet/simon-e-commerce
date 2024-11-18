ba5ddd24404d430408e75b9315e59ff338961efe
rmymartinet <remymartinet.contact@gmail.com>
2024-11-18 10:02:15 +0100

    Update global styles and tailwind configuration

--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -15,11 +15,17 @@ const config: Config = {
       gridTemplateColumns: {
         custom: "20vw minmax(20vw, 1fr) minmax(20vw, 1fr)",
       },
+      gridTemplateRows: {
+        cardPrice: "150px max-content max-content",
+      },
       gridColumn: {
         "start-1-end-2": "1 / 2",
         "start-2-end-4": "2 / 4",
         "start-2-end-3": "2 / 3",
         "start-3-end-4": "3 / 4",
+      },
+      backgroundColor: {
+        glassmorph: "rgba(255, 255, 255, 0.35)",
       },
     },
   },
