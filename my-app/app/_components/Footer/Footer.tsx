ecbe38832d707e8f621d84fceec88d832ca6fc0c
rmymartinet <remymartinet.contact@gmail.com>
2024-11-23 15:05:19 +0100

    Refactor Footer component: update background class for improved animation effect

--- my-app/app/_components/Footer/Footer.tsx
+++ my-app/app/_components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import FooterLinks from "./FooterLinks";
 const Footer = () => {
   return (
     <footer className="relative mt-40 flex w-full justify-between bg-[#D1D1D133] px-40 py-20 text-sm text-white">
-      <div className="test-f absolute bottom-0 right-0 -z-20 h-[80%] w-full" />
+      <div className="animate-bg-purple absolute bottom-0 right-0 -z-20 h-[80%] w-full" />
       <div className="flex flex-col justify-between gap-2">
         <div className="flex flex-col gap-2">
           <h3 className="text-2xl font-semibold">SM Coaching</h3>
