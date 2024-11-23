3521b6350862ad46ef6b6182be9a01a07f185516
rmymartinet <remymartinet.contact@gmail.com>
2024-11-23 18:54:28 +0100

    Improved the Footer component with a new glassmorph  style and reorganized the content.

--- my-app/app/_components/Footer/Footer.tsx
+++ my-app/app/_components/Footer/Footer.tsx
@@ -1,19 +1,28 @@
+import FooterContact from "./FooterContact";
 import FooterLinks from "./FooterLinks";
 
 const Footer = () => {
   return (
-    <footer className="relative mt-40 flex w-full justify-between bg-[#D1D1D133] px-40 py-20 text-sm text-white">
+    <footer className="footer-glassmorph relative mt-40 flex min-h-[90vh] w-full flex-col justify-between px-10 py-10 text-sm text-white lg:px-40 lg:py-10">
       <div className="animate-bg-purple absolute bottom-0 right-0 -z-20 h-[80%] w-full" />
-      <div className="flex flex-col justify-between gap-2">
-        <div className="flex flex-col gap-2">
-          <h3 className="text-2xl font-semibold">SM Coaching</h3>
-          <button className="w-max rounded-md bg-button px-4 py-2 text-white hover:bg-[#9D4EDD]">
-            Commencer
-          </button>
-        </div>
-        {/* <FooterContact /> */}
+      <div className="flex flex-col items-center gap-40">
+        <h1 className="text-5xl font-bold uppercase md:text-8xl lg:text-[12rem]">
+          SM Coaching
+        </h1>
+        <div className="flex flex-col gap-2"></div>
       </div>
-      <FooterLinks />
+      <div className="flex flex-col justify-between gap-20">
+        <div className="flex flex-col justify-between gap-20 lg:flex-row lg:gap-0">
+          <div className="flex flex-col gap-6">
+            <h1 className="text-2xl">Commencer dès maintenant</h1>
+            <button className="h-max w-max rounded-lg bg-button p-2 px-4 font-semibold">
+              Commencer
+            </button>
+          </div>
+          <FooterLinks />
+        </div>
+        <FooterContact />
+      </div>
     </footer>
   );
 };
