36c58189a130b2c8c4ad5e53edfd3962388c20d2
rmymartinet <remymartinet.contact@gmail.com>
2024-11-23 14:49:41 +0100

    Update the Header component to change the background video and add animations to the OfferSelector component.

--- my-app/app/_components/Header/Header.tsx
+++ my-app/app/_components/Header/Header.tsx
@@ -6,14 +6,15 @@ gsap.registerPlugin(useGSAP);
 
 const Header = () => {
   return (
-    <div className="relative flex min-h-screen items-center justify-center p-8">
+    <div className="relative flex min-h-screen w-screen items-center justify-center p-8">
       <div className="absolute h-full w-full overflow-hidden">
         <video
           autoPlay
           loop
           muted
-          src="/videos/text.mp4"
+          src="/videos/yo.mp4"
           className="h-full w-full object-cover brightness-[0.9] filter"
+          preload="true"
         ></video>
         <OfferSelector />
       </div>
--- my-app/app/_components/Header/OfferSelector.tsx
+++ my-app/app/_components/Header/OfferSelector.tsx
@@ -1,8 +1,36 @@
+import { useAnimation } from "@/app/AnimationContext";
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
 import { IoIosArrowRoundForward } from "react-icons/io";
 
 const OfferSelector = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const { isAnimating } = useAnimation();
+
+  useEffect(() => {
+    if (!isAnimating) {
+      gsap.to(containerRef.current, {
+        y: 0,
+        opacity: 1,
+        delay: 0.2,
+        duration: 1,
+        ease: "power3.inOut",
+      });
+    } else {
+      gsap.to(containerRef.current, {
+        y: 100,
+        opacity: 0,
+        duration: 1,
+        ease: "power3.inOut",
+      });
+    }
+  }, [isAnimating]);
   return (
-    <div className="absolute bottom-10 left-1/2 flex w-full -translate-x-1/2 flex-col items-center rounded-3xl border-2 border-slate-100 bg-black p-4 text-black shadow-2xl md:w-max">
+    <div
+      ref={containerRef}
+      className="absolute bottom-10 left-1/2 flex w-full -translate-x-1/2 flex-col items-center rounded-3xl border-2 border-slate-100 bg-black p-4 text-black opacity-0 shadow-2xl md:w-max"
+    >
       <div className="grid grid-cols-2 gap-4 overflow-hidden">
         <div className="rounded-xl bg-white p-2">
           <div className="w-42 flex flex-col justify-between gap-4 p-2">
@@ -10,7 +38,7 @@ const OfferSelector = () => {
               <h1 className="text-xl text-black">Programme</h1>
               <p className="text-slate-300">Accès à vie, sans suivi</p>
             </div>
-            <button className="flex w-full items-center justify-between rounded-xl bg-black px-4 py-2 text-white">
+            <button className="flex w-full items-center justify-between rounded-md bg-black px-4 py-2 text-white">
               <p>Commencer</p>
               <div className="-rotate-45">
                 <IoIosArrowRoundForward size={20} />
@@ -24,7 +52,7 @@ const OfferSelector = () => {
               <h1 className="text-xl text-black">Coaching</h1>
               <p className="text-slate-300">Accompagnement sur-mesure</p>
             </div>
-            <button className="flex w-full items-center justify-between rounded-xl bg-black px-4 py-2 text-white">
+            <button className="flex w-full items-center justify-between rounded-md bg-black px-4 py-2 text-white">
               <p>Commencer</p>
               <div className="-rotate-45">
                 <IoIosArrowRoundForward size={20} />
