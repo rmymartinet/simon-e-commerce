fd6f63685e96a421b3c01a9a817c0c6109269c5b
rmymartinet <remymartinet.contact@gmail.com>
2024-12-03 23:26:49 +0100

    Added daily price management in the Price component and updated price display based on subscription duration.

--- my-app/app/_components/Card/Price.tsx
+++ my-app/app/_components/Card/Price.tsx
@@ -1,14 +1,28 @@
-const Price = ({ price, mounth }: { price: string; mounth?: string }) => (
+const Price = ({
+  price,
+  mounth,
+  dayPrice,
+}: {
+  price: string;
+  mounth?: string;
+  dayPrice?: string;
+}) => (
   <div className="flex flex-col gap-2">
     <div className="flex flex-1 items-center">
       <span className="mr-2 self-start text-2xl">€</span>
       <span className="mr-2 text-8xl font-semibold">{price}</span>
-      <p className="self-end text-base">/ mois</p>
+      {mounth ? (
+        <p className="self-end text-base">/ mois</p>
+      ) : (
+        <p className="self-end text-base">Disponible à vie</p>
+      )}
     </div>
     {mounth && (
-      <p className="text-sm text-slate-300">
-        Valable sur une durée de {mounth}
-      </p>
+      <div>
+        <p className="pl-2 text-base font-semibold text-[#E0AAFF]">
+          À moins de {dayPrice} €/jour
+        </p>
+      </div>
     )}
   </div>
 );
