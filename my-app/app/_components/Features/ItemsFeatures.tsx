cd2cd2637e04c4f6e9063aa03d76d9f34128da50
rmymartinet <remymartinet.contact@gmail.com>
2024-12-07 20:19:20 +0100

    Refactor ItemsFeatures component to improve layout and styling, including type definition adjustments and centering text on smaller screens.

--- my-app/app/_components/Features/ItemsFeatures.tsx
+++ my-app/app/_components/Features/ItemsFeatures.tsx
@@ -1,15 +1,10 @@
 import { revealRotateEl } from "@/app/utils/Animation";
+import { ItemsFeaturesProps } from "@/types/types";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { useRef } from "react";
 
 gsap.registerPlugin(useGSAP);
-
-interface ItemsFeaturesProps {
-  logo: React.ReactNode;
-  title: string;
-  paragraph: string;
-}
 
 const ItemsFeatures = ({ logo, title, paragraph }: ItemsFeaturesProps) => {
   const containerRef = useRef(null);
@@ -19,10 +14,13 @@ const ItemsFeatures = ({ logo, title, paragraph }: ItemsFeaturesProps) => {
   }, []);
 
   return (
-    <div ref={containerRef} className="flex flex-col gap-4">
+    <div
+      ref={containerRef}
+      className="flex flex-col items-center gap-4 md:items-start"
+    >
       <div>{logo}</div>
       <h2 className="text-2xl">{title}</h2>
-      <p className="w-full text-pretty text-slate-400 lg:w-[24vw]">
+      <p className="w-full text-pretty text-center text-slate-400 md:text-start lg:w-[24vw]">
         {paragraph}
       </p>
     </div>
