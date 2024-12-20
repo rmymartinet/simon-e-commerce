5a9b52c6b77cb32cdbb140faf065f1171daa01bd
rmymartinet <remymartinet.contact@gmail.com>
2024-12-20 12:00:16 +0100

    Add background image and enhance Faq component styling

--- my-app/app/_components/Faq.tsx
+++ my-app/app/_components/Faq.tsx
@@ -1,16 +1,26 @@
+import Image from "next/image";
 import { faqData } from "../data/faq";
 import Accordion from "./Accordion";
 
 const Faq = () => {
   return (
     <section className="relative self-center lg:w-[70%]">
-      <div className="mb-20 flex flex-col items-center justify-center gap-4">
-        <h1 className="text-7xl">Faq</h1>
-        <p className="text-slate-400">
+      <div className="absolute left-0 top-[20%] -z-10 h-[60%] w-[90%] -translate-x-1/2">
+        <Image
+          src="/images/cube_mono.jpeg"
+          alt=""
+          width={2000}
+          height={2000}
+          className="h-max w-max object-contain"
+        />
+      </div>
+      <div className="mb-20 flex flex-col items-center justify-center">
+        <h1 className="text-7xl font-bold">Faq</h1>
+        <p className="text-lg font-semibold text-textOpacity">
           Les questions les plus fréquemment posées.
         </p>
       </div>
-      <div className="overflow-hidden rounded-2xl border border-slate-200">
+      <div className="flex flex-col gap-4 overflow-hidden">
         {faqData.map((item, index) => (
           <Accordion key={index} title={item.question} text={item.answer} />
         ))}
