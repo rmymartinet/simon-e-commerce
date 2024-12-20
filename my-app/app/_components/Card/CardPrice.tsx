ab8cf1a2e6e9d8d70fa23683ac8e29e6efc140a8
rmymartinet <remymartinet.contact@gmail.com>
2024-12-20 12:58:12 +0100

    Refactor CardPriceContainer: update coaching and program data structures, enhance layout with Stars component, and streamline rendering logic.

--- my-app/app/_components/Card/CardPrice.tsx
+++ my-app/app/_components/Card/CardPrice.tsx
@@ -1,63 +1,239 @@
+"use client";
+
 import { CardPriceProps } from "@/types/types";
 import { IoCheckmarkOutline } from "react-icons/io5";
 import Price from "./Price";
-import useWindowWidth from "@/app/hooks/useWindowWidth";
+import useWindowWidth from "@/hooks/useWindowWidth";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { loadStripe } from "@stripe/stripe-js";
+import Divider from "../Divider";
 
 const CardPrice = ({
+  priceMounthly,
+  priceId,
+  filterName,
   title,
+  mostPopular,
   mounth,
   price,
-  text,
+  description,
   includes,
   bgColor,
   dayPrice,
 }: CardPriceProps) => {
   const { width } = useWindowWidth();
+  const isMobileDefault = width <= 768;
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [userData, setUserData] = useState<any>(null);
+  const [userId, setUserId] = useState<string | null>(null);
+  const [isMobile, setIsMobile] = useState(isMobileDefault);
+  const [showConnexion, setShowConnexion] = useState(false);
 
-  const isMobile = width <= 768;
+  const stripePromise = loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!,
+  );
+
+  useEffect(() => {
+    setIsMobile(width <= 768);
+  }, [width]);
+
+  useEffect(() => {
+    const checkAuthStatus = async () => {
+      try {
+        const res = await fetch("/api/auth/check-auth");
+        const data = await res.json();
+        setIsAuthenticated(data.isAuthenticated);
+        setUserId(data.user);
+      } catch (error) {
+        console.error("Failed to check authentication:", error);
+      }
+    };
+
+    checkAuthStatus();
+  }, []);
+
+  useEffect(() => {
+    if (isAuthenticated && userId) {
+      const fetchData = async () => {
+        try {
+          const res = await fetch(`api/${userId}`, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+
+          const userData = await res.json();
+
+          setUserData(userData);
+        } catch (e) {
+          console.error(e);
+        }
+      };
+      fetchData();
+    }
+  }, [isAuthenticated, userId]);
+
+  const handleCheckout = async (
+    priceId: string,
+    subscription: boolean,
+    guest: boolean,
+  ) => {
+    try {
+      const response = await fetch(`/api/payments/create-checkout-session`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: userId,
+          priceId,
+          subscription,
+          guest,
+          mounth: mounth || 0,
+          titlePlan: title,
+          email: userData?.email,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (data.sessionId) {
+        const stripe = await stripePromise;
+
+        const response = await stripe?.redirectToCheckout({
+          sessionId: data.sessionId,
+        });
+
+        return response;
+      } else {
+        console.error("Failed to create checkout session");
+        return;
+      }
+    } catch (error) {
+      console.error("Error during checkout:", error);
+      return;
+    }
+  };
+
+  const handleAction = () => {
+    switch (true) {
+      case isAuthenticated && filterName === "coaching":
+        if (priceMounthly) {
+          handleCheckout(priceMounthly, true, false);
+        } else {
+          console.error("priceMounthly is undefined");
+        }
+        break;
+
+      case isAuthenticated && filterName === "programmes":
+        if (priceId) {
+          handleCheckout(priceId, false, false);
+        } else {
+          console.error("priceId is undefined");
+        }
+        break;
+
+      case !isAuthenticated && filterName === "coaching":
+        setShowConnexion(true);
+        break;
+
+      case !isAuthenticated && filterName === "programmes":
+        setShowConnexion(true);
+        break;
+
+      default:
+        console.error("Action non prise en charge");
+    }
+  };
 
   return (
-    <div
-      className={`relative flex flex-col rounded-3xl px-10 py-7 ${
-        bgColor
-          ? `${!isMobile ? "scale-105" : ""} bg-[#10002B] text-white`
-          : "bg-[#F8F8F8] text-black"
-      }`}
-    >
-      {bgColor && (
-        <>
-          <div className="popular-box absolute -right-10 -top-4 w-max -translate-x-1/2 rounded-full p-2 font-semibold">
-            Meilleur choix
+    <>
+      {showConnexion && (
+        <div className="fixed bottom-4 right-4 z-[9999] flex items-center gap-10 rounded-lg border-card bg-[#0b0d1e] p-6 shadow-xl transition-transform duration-300 ease-in-out">
+          <div className="flex max-w-[400px] flex-col gap-2">
+            <p className="text-sm font-medium">
+              {filterName === "coaching"
+                ? "Connectez-vous ou inscrivez-vous pour acheter."
+                : "Connectez-vous ou continuez en tant qu'invité pour payer."}
+            </p>
+            <p className="text-sm text-slate-400">
+              Vous devez être connecté pour effectuer un achat.
+            </p>
           </div>
-        </>
+          <div className="flex flex-col gap-4">
+            <Link href="/login">
+              <button className="bg-button rounded-md px-2 py-1 text-sm text-white">
+                Se connecter
+              </button>
+            </Link>
+            {filterName !== "coaching" && (
+              <button
+                className="rounded-md bg-gray-300 px-2 py-1 text-sm text-white"
+                onClick={() => {
+                  if (priceId) {
+                    handleCheckout(priceId, false, true);
+                  } else {
+                    console.error("priceId is undefined");
+                  }
+                }}
+              >
+                Continuer en tant qu'invité
+              </button>
+            )}
+          </div>
+        </div>
       )}
-      <div className="mb-20 flex justify-between">
-        <h1 className="text-4xl font-semibold">{title}</h1>
-        <div className="flex flex-col gap-4">
-          {mounth && (
-            <span className="flex w-max items-center justify-center rounded-full px-4 py-2 shadow-inner">
-              Durée {mounth}
-            </span>
-          )}
+
+      <div
+        className={`glo card relative flex flex-col rounded-3xl p-10 transition-transform duration-300 ease-in-out lg:w-[30vw] ${
+          mostPopular
+            ? `${!isMobile ? "scale-105 border border-[#1F26875E]" : ""} z-50`
+            : ""
+        }`}
+        style={{ willChange: "transform", backfaceVisibility: "hidden" }}
+      >
+        {mostPopular && (
+          <div className="popular-box absolute -right-10 -top-4 w-max -translate-x-1/2 rounded-full p-2 font-semibold">
+            Plus populaire
+          </div>
+        )}
+        <div className="mb-20 flex justify-between">
+          <h1 className="text-4xl font-semibold">{title}</h1>
+        </div>
+        <Price price={price} mounth={mounth} dayPrice={dayPrice} />
+        <div className="mb-2 mt-4">
+          <Divider bgColor="text-slate-400" />
+        </div>
+
+        <div className="grid grid-rows-cardPrice">
+          <p className="mt-2 text-center md:text-start">{description}</p>
+          <div className="mb-10 flex flex-col gap-4">
+            <button
+              className="box bg-button-gradient bg-line padding w-full self-end rounded-[1.125rem] text-center text-lg font-semibold text-white"
+              onClick={handleAction}
+            >
+              Acheter
+            </button>
+            <button className="bg-button-2 padding w-full self-end rounded-[1.125rem] text-center text-lg font-semibold text-white">
+              Ajouter au panier
+            </button>
+          </div>
+          <ul className="flex flex-col gap-3">
+            <p className="text-slate-300">Inclus:</p>
+            {includes?.map((include, index) => (
+              <li key={index} className="flex items-center gap-2">
+                <div className="card rounded-full p-1">
+                  <IoCheckmarkOutline color="violet" />
+                </div>
+                {include}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
-      <Price price={price} mounth={mounth} dayPrice={dayPrice} />
-      <div className="grid grid-rows-cardPrice">
-        <p className="mt-10 text-center md:text-start">{text}</p>
-        <button className="mb-10 w-full self-end rounded-xl bg-button px-4 py-4 text-center text-lg font-semibold text-white">
-          Acheter
-        </button>
-        <ul className="flex flex-col gap-3">
-          <p className="text-slate-300">Inclus:</p>
-          {includes?.map((include, index) => (
-            <li key={index} className="flex items-center gap-2">
-              <IoCheckmarkOutline />
-              {include}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    </>
   );
 };
 
--- my-app/app/_components/Card/CardPriceContainer.tsx
+++ my-app/app/_components/Card/CardPriceContainer.tsx
@@ -4,69 +4,139 @@ import {
   coachingIncludes9Mounth,
   programIncludes,
 } from "../../data/cardPrice";
+import Stars from "../Stars";
 import CardPrice from "./CardPrice";
 
+export const coachingData = [
+  {
+    title: "Essai",
+    mounth: 3,
+    dayPrice: "5",
+    price: "147",
+    description:
+      "Testez nos services et observez les premiers changements. Parfait pour initier votre transformation de style de vie.",
+    priceMounthly: process.env.NEXT_PUBLIC_STRIPE_PRICE_ID!,
+  },
+  {
+    title: "Standard",
+    mounth: 6,
+    dayPrice: "4,50",
+    price: "127",
+    description:
+      "Ancrez des habitudes saines et obtenez des résultats durables. Le meilleur équilibre entre durée et efficacité.",
+    priceMounthly: process.env.NEXT_PUBLIC_STRIPE_PRICE_ID!,
+    mostPopular: true,
+  },
+  {
+    title: "Intensif",
+    mounth: 9,
+    dayPrice: "4",
+    price: "107",
+    description:
+      "Transformez-vous totalement avec un suivi intensif. L'engagement ultime pour un changement profond et pérenne.",
+    priceMounthly: process.env.NEXT_PUBLIC_STRIPE_PRICE_ID!,
+  },
+];
+export const programData = [
+  {
+    title: "Débutant",
+    price: "77",
+    description:
+      "Un programme simple pour bien démarrer et poser les bases d’une nouvelle routine. Idéal pour débuter en douceur",
+
+    priceId: process.env.NEXT_PUBLIC_STRIPE_PROGRAM_BEGINNER_PRICE_ID!,
+  },
+  {
+    title: "Intermédiaire",
+    price: "87",
+    description:
+      "Consolidez vos acquis et progressez à votre rythme. Une approche équilibrée pour des résultats visibles",
+
+    priceId: process.env.NEXT_PUBLIC_STRIPE_PROGRAM_BEGINNER_PRICE_ID!,
+    mostPopular: true,
+  },
+  {
+    title: "Avancé",
+    price: "97",
+    description:
+      "Pour ceux qui veulent aller plus loin : un programme complet pour des objectifs ambitieux",
+
+    priceId: process.env.NEXT_PUBLIC_STRIPE_PROGRAM_BEGINNER_PRICE_ID!,
+  },
+];
 const CardPriceContainer = ({ filterName }: { filterName: string }) => {
   return (
     <>
       {filterName === "programmes" && (
-        <div className="relative flex flex-col gap-10 px-4 lg:grid lg:grid-cols-3 xl:w-[80%]">
-          <div className="fixed-bg-purple absolute bottom-0 right-0 -z-10 h-full w-full"></div>
+        <div className="relative flex flex-col place-content-center gap-4 px-4 md:gap-0 md:px-0 lg:grid lg:grid-cols-3">
+          <div className="animate-bg-purple absolute bottom-0 right-0 -z-10 h-full w-full"></div>
+          <Stars
+            yposition="-top-10"
+            xposition="left-0"
+            height="h-20"
+            weight="w-full"
+            isTop={false}
+          />
+          <Stars
+            yposition="-bottom-10"
+            xposition="left-0"
+            height="h-20"
+            weight="w-full"
+            isTop={false}
+          />
+          <Stars
+            yposition="top-0"
+            xposition="-right-10"
+            height="h-full"
+            weight="w-20"
+            isTop={true}
+          />
 
-          <CardPrice
-            title="Débutant"
-            price="77"
-            text="Un programme simple pour bien démarrer et poser les bases d’une nouvelle routine. Idéal pour débuter en douceur"
-            includes={programIncludes}
+          <Stars
+            yposition="top-0"
+            xposition="-left-10"
+            height="h-full"
+            weight="w-20"
+            isTop={true}
           />
-          <CardPrice
-            bgColor
-            title="Intermédiaire"
-            price="87"
-            text="Consolidez vos acquis et progressez à votre rythme. Une approche équilibrée pour des résultats visibles"
-            discount="Économisez 16%"
-            includes={programIncludes}
-          />
-          <CardPrice
-            title="Avancé"
-            price="97"
-            text="Pour ceux qui veulent aller plus loin : un programme complet pour des objectifs ambitieux"
-            discount="Économisez 23%"
-            includes={programIncludes}
-          />
+
+          {programData.map((data, index) => (
+            <CardPrice
+              key={data.title}
+              priceId={data.priceId}
+              mostPopular={data.mostPopular}
+              filterName={filterName}
+              title={data.title}
+              price={data.price}
+              description={data.description}
+              includes={programIncludes}
+            />
+          ))}
         </div>
       )}
       {filterName === "coaching" && (
-        <div className="relative flex flex-col gap-10 px-2 md:px-20 lg:grid lg:w-[80%] lg:grid-cols-3 lg:px-0">
+        <div className="relative flex flex-col place-content-center lg:grid lg:grid-cols-3">
           <div className="fixed-bg-purple absolute bottom-0 right-0 -z-10 h-full w-full"></div>
-
-          <CardPrice
-            title="Essai"
-            mounth="3 mois"
-            dayPrice="5"
-            price="147"
-            text="Testez nos services et observez les premiers changements. Parfait pour initier votre transformation de style de vie."
-            includes={coachingIncludes3Mounth}
-          />
-          <CardPrice
-            bgColor
-            title="Standard"
-            mounth="6 mois"
-            dayPrice="4,50"
-            price="127"
-            text="Ancrez des habitudes saines et obtenez des résultats durables. Le meilleur équilibre entre durée et efficacité."
-            discount="Economisez 16%"
-            includes={coachingIncludes6Mounth}
-          />
-          <CardPrice
-            title="Intensif"
-            mounth="9 mois"
-            dayPrice="4"
-            price="107"
-            text="Transformez-vous totalement avec un suivi intensif. L'engagement ultime pour un changement profond et pérenne."
-            discount="Economisez 23%"
-            includes={coachingIncludes9Mounth}
-          />
+          {coachingData.map((data, index) => (
+            <CardPrice
+              key={data.title}
+              priceMounthly={data.priceMounthly}
+              filterName={filterName}
+              title={data.title}
+              dayPrice={data.dayPrice}
+              mostPopular={data.mostPopular}
+              price={data.price}
+              description={data.description}
+              mounth={data.mounth}
+              includes={
+                data.mounth === 3
+                  ? coachingIncludes3Mounth
+                  : data.mounth === 6
+                    ? coachingIncludes6Mounth
+                    : coachingIncludes9Mounth
+              }
+            />
+          ))}
         </div>
       )}
     </>
