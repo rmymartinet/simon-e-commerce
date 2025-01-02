3606c258a069f9299cc31df2a6dd8a32ba53afeb
rmymartinet <remymartinet.contact@gmail.com>
2025-01-02 11:47:04 +0100

    Refactor Iphone component: adjust height properties for improved layout and responsiveness

--- my-app/app/_components/Iphone.tsx
+++ my-app/app/_components/Iphone.tsx
@@ -14,7 +14,7 @@ const Iphone = ({
       {title && (
         <h1 className="card padding rounded-xl text-xl font-bold">{title}</h1>
       )}{" "}
-      <div className="relative z-50 h-[30vh] md:h-[50vh]">
+      <div className="relative z-50 h-[50vh]">
         <div className="relative h-full w-full overflow-hidden">
           <Image
             src="/images/iphonee.png"
@@ -30,12 +30,12 @@ const Iphone = ({
               alt=""
               width={1000}
               height={1000}
-              className="absolute inset-0 left-1/2 top-1/2 -z-10 flex h-[80%] w-[87%] -translate-x-1/2 -translate-y-1/2 items-center justify-center object-cover lg:h-[95%] lg:w-[88%]"
+              className="absolute inset-0 left-1/2 top-1/2 -z-10 flex h-[90%] w-[87%] -translate-x-1/2 -translate-y-1/2 items-center justify-center object-cover lg:h-[95%] lg:w-[88%]"
               quality={100}
             />
           )}
           {video && (
-            <div className="absolute inset-0 left-1/2 top-1/2 -z-10 flex h-[80%] w-[87%] -translate-x-1/2 -translate-y-1/2 items-center justify-center lg:h-[95%] lg:w-[88%]">
+            <div className="absolute inset-0 left-1/2 top-1/2 -z-10 flex h-[95%] w-[87%] -translate-x-1/2 -translate-y-1/2 items-center justify-center lg:h-[95%] lg:w-[88%]">
               <video
                 autoPlay
                 playsInline
