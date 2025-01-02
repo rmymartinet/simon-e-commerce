49d0905813fb8f032934aba38498ad658bc75488
rmymartinet <remymartinet.contact@gmail.com>
2025-01-02 11:46:51 +0100

    Refactor ColorShadowButton: integrate navigation utility for pricing and adjust button styles

--- my-app/app/_components/ColorShadowButton.tsx
+++ my-app/app/_components/ColorShadowButton.tsx
@@ -1,3 +1,5 @@
+import useNavigation from "@/utils/navigationUtils";
+
 const ColorShadowButton = ({
   title,
   color,
@@ -5,13 +7,19 @@ const ColorShadowButton = ({
   title: string;
   color: string;
 }) => {
+  const { handlePricingNavigation } = useNavigation();
+
   return (
-    <div className="relative mt-4 w-max">
-      <button className="program-button-container padding rounded-xl bg-button-gradient font-bold">
+    <div className="relative mt-8 w-max">
+      <button
+        onClick={handlePricingNavigation}
+        className="program-button-container padding rounded-xl bg-button-gradient font-bold"
+      >
         {title}
       </button>
       <div
-        className={`absolute inset-0 -z-10 h-full w-full rounded-xl bg-[${color}] blur-sm`}
+        className={`absolute inset-0 -z-10 h-full w-full rounded-xl blur-sm`}
+        style={{ backgroundColor: color }}
       />
       <div className="absolute inset-0 -z-10 h-full w-full rounded-xl bg-[#0b0d14]" />
     </div>
