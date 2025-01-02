7d4c7014ff9d66e726dfa37af514c93b55a74594
rmymartinet <remymartinet.contact@gmail.com>
2025-01-02 11:52:30 +0100

    Refactor Footer component: integrate navigation utility for pricing button and enhance button functionality

--- my-app/app/_components/Footer/Footer.tsx
+++ my-app/app/_components/Footer/Footer.tsx
@@ -1,7 +1,9 @@
+import useNavigation from "@/utils/navigationUtils";
 import FooterContact from "./FooterContact";
 import FooterLinks from "./FooterLinks";
 
 const Footer = () => {
+  const { handlePricingNavigation } = useNavigation();
   return (
     <footer className="relative mt-40 flex h-[200vdh] w-full flex-col justify-between px-10 py-10 text-sm text-white lg:px-40 lg:py-10">
       <div className="animate-bg-purple absolute bottom-0 right-0 -z-20 h-[40%] w-full" />
@@ -12,7 +14,10 @@ const Footer = () => {
             <h1 className="text-2xl font-bold md:text-3xl">
               Commencer dès maintenant
             </h1>
-            <button className="padding h-max w-max rounded-lg bg-button-gradient text-xl">
+            <button
+              onClick={handlePricingNavigation}
+              className="padding h-max w-max rounded-lg bg-button-gradient text-xl"
+            >
               Commencer
             </button>
           </div>
