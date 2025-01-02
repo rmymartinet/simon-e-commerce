e4527fb99cc1f2a85ccf646c5abbeb6a1b4977f4
rmymartinet <remymartinet.contact@gmail.com>
2025-01-02 10:23:51 +0100

    Refactor NutritionAdvice component and extract data to a separate file for better organization

--- my-app/app/_components/program/NutritionAdvice.tsx
+++ my-app/app/_components/program/NutritionAdvice.tsx
@@ -2,65 +2,12 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useRef } from "react";
+import ColorShadowButton from "../ColorShadowButton";
+import { data } from "@/app/data/nutritionAdviceData";
 
 gsap.registerPlugin(ScrollTrigger, useGSAP);
 
-const data = [
-  {
-    title: "Répartitissez vos aliments",
-    description: (
-      <>
-        Que ce soit pour{" "}
-        <span className="font-semibold text-violet-500">perdre du poids</span>,{" "}
-        <span className="font-semibold text-violet-500">
-          prendre de la masse
-        </span>{" "}
-        ou{" "}
-        <span className="font-semibold text-violet-500">
-          maintenir votre forme
-        </span>
-        , apprenez à structurer vos repas en fonction de vos objectifs et de
-        votre mode de vie. Planifiez vos repas pré- et post-entraînement pour
-        optimiser vos performances et vos résultats, tout en ajustant vos
-        collations à vos besoins.
-      </>
-    ),
-  },
-  {
-    title: "Composez vos repas comme un expert",
-    description: (
-      <>
-        Apprenez à sélectionner les bons aliments pour équilibrer vos
-        macronutriments ({" "}
-        <span className="font-semibold text-violet-500">glucides</span>,{" "}
-        <span className="font-semibold text-violet-500">protéines</span>,{" "}
-        <span className="font-semibold text-violet-500">lipides</span> ) et
-        micronutriments. Chaque repas est conçu pour respecter vos besoins
-        spécifiques. Exemple : si votre petit-déjeuner nécessite{" "}
-        <span className="font-semibold text-violet-500">200 g de glucides</span>
-        , découvrez comment choisir les aliments adaptés tout en respectant vos
-        besoins en protéines et lipides.
-      </>
-    ),
-  },
-  {
-    title: "Des menus savoureux et adaptés à vos objectifs",
-    description: (
-      <>
-        Choisissez des recettes savoureuses qui respectent vos{" "}
-        <span className="font-semibold text-violet-500">
-          objectifs nutritionnels
-        </span>
-        . Ajustez vos repas pour un équilibre optimal, que ce soit en cuisinant
-        maison pour plus de contrôle ou en planifiant avec du{" "}
-        <span className="font-semibold text-violet-500">meal-prep</span>. Vous
-        aurez toutes les clés pour manger équilibré, tout en prenant du plaisir.
-      </>
-    ),
-  },
-];
-
-const ProgramNutrition = () => {
+const NutritionAdvice = () => {
   const numsRefs = useRef<(HTMLDivElement | null)[]>([]);
 
   useGSAP(() => {
@@ -82,20 +29,14 @@ const ProgramNutrition = () => {
 
   return (
     <div className="mt-[20vh] flex w-screen flex-col items-center gap-20">
-      <div className="mb-20 flex flex-col items-center text-center md:mb-[10vh]">
+      <div className="mb-20 flex flex-col items-center px-4 text-center md:mb-[10vh]">
         <h1 className="text-xl font-bold lg:text-3xl">
           Une nutrition adaptée pour des résultats durables
         </h1>
-        <p className="font-medium text-textOpacity md:text-lg">
+        <p className="font-medium text-subtle md:text-lg">
           Manger intelligemment, c’est 50% du chemin vers vos objectifs
         </p>
-        <div className="relative mt-4 w-max">
-          <button className="program-button-container padding rounded-xl font-bold">
-            Voir les offres
-          </button>
-          <div className="absolute inset-0 -z-10 h-full w-full rounded-xl bg-[#f690ff] blur-sm"></div>
-          <div className="absolute inset-0 -z-10 h-full w-full rounded-xl bg-[#0b0d14]"></div>
-        </div>
+        <ColorShadowButton title="Voir les offres" color="#f690ff" />
       </div>
       <div className="relative flex flex-col items-center gap-20">
         {data.map((item, index) => (
@@ -105,7 +46,7 @@ const ProgramNutrition = () => {
           >
             <div className="flex flex-col gap-2 text-center lg:text-start">
               <h1 className="text-2xl font-bold">{item.title}</h1>
-              <p className="text-pretty break-words text-lg text-textOpacity">
+              <p className="text-pretty break-words text-lg text-muted">
                 {item.description}
               </p>
             </div>
@@ -124,4 +65,4 @@ const ProgramNutrition = () => {
   );
 };
 
-export default ProgramNutrition;
+export default NutritionAdvice;
---
+++ my-app/app/data/nutritionAdviceData.tsx
@@ -0,0 +1,51 @@
+export const data = [
+  {
+    title: "Répartitissez vos aliments",
+    description: (
+      <>
+        Que ce soit pour{" "}
+        <span className="font-semibold text-white">perdre du poids</span>,{" "}
+        <span className="font-semibold text-white">prendre de la masse</span> ou{" "}
+        <span className="font-semibold text-white">maintenir votre forme</span>,
+        apprenez à structurer vos repas en fonction de vos objectifs et de votre
+        mode de vie. Planifiez vos repas pré- et post-entraînement pour
+        optimiser vos{" "}
+        <span className="font-semibold text-white">performances</span> et vos{" "}
+        <span className="font-semibold text-white">résultats</span>, tout en
+        ajustant vos collations à vos besoins.
+      </>
+    ),
+  },
+  {
+    title: "Composez vos repas comme un expert",
+    description: (
+      <>
+        Apprenez à sélectionner les bons aliments pour équilibrer vos
+        macronutriments ({" "}
+        <span className="font-semibold text-white">glucides</span>,{" "}
+        <span className="font-semibold text-white">protéines</span>,{" "}
+        <span className="font-semibold text-white">lipides</span> ) et
+        micronutriments. Chaque repas est conçu pour respecter vos besoins
+        spécifiques. Exemple : si votre petit-déjeuner nécessite{" "}
+        <span className="font-semibold text-white">200 g de glucides</span>,
+        découvrez comment choisir les aliments adaptés tout en respectant vos
+        besoins en protéines et lipides.
+      </>
+    ),
+  },
+  {
+    title: "Des menus savoureux et adaptés à vos objectifs",
+    description: (
+      <>
+        Choisissez des recettes savoureuses qui respectent vos{" "}
+        <span className="font-semibold text-white">
+          objectifs nutritionnels
+        </span>
+        . Ajustez vos repas pour un équilibre optimal, que ce soit en cuisinant
+        maison pour plus de contrôle ou en planifiant avec du{" "}
+        <span className="font-semibold text-white">meal-prep</span>. Vous aurez
+        toutes les clés pour manger équilibré, tout en prenant du plaisir.
+      </>
+    ),
+  },
+];
