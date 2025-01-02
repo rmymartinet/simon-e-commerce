d258d4834acbff4895dad8fc340b735a2bfb30cb
rmymartinet <remymartinet.contact@gmail.com>
2025-01-02 11:34:27 +0100

    Delete the feedback.tsx and faq.tsx files, and create feedbackData.tsx with new feedback data for better data organization.

---
+++ my-app/app/data/faqData.tsx
@@ -0,0 +1,186 @@
+export const faqCoachingData = [
+  {
+    question:
+      "Pouvez-vous me présenter les différentes formules mises en vente ?",
+    answer: `
+      3 formules possibles sport et nutrition :<br><br>   
+      <ul class="py-2 list-inside list-disc">
+        <li>Formule 3 mois : Coaching personnalisé avec suivi hebdomadaire pour amorcer votre transformation.</li>
+        <li>Formule 6 mois : Pour une évolution significative avec un suivi continu.</li>
+        <li>Formule 9 mois : Coaching complet pour des résultats durables.</li>
+      </ul><br>
+    `,
+  },
+  {
+    question: "Quelle formule me correspond le mieux ?",
+    answer: `
+     
+      <ul class="list-inside list-disc">
+        <li><strong>Formule 3 mois :</strong> Idéale pour débuter en douceur.</li>
+        <li><strong>Formule 6 mois :</strong> Pour des résultats à moyen terme.</li>
+        <li><strong>Formule 9 mois :</strong> Engagement à long terme pour une transformation durable.</li>
+      </ul><br>
+    `,
+  },
+  {
+    question:
+      "Pourquoi choisir un coaching personnalisé plutôt qu'un programme déjà fait et moins cher ?",
+    answer: `
+      <strong>Les avantages du coaching personnalisé :</strong><br>
+      <ul class="list-inside list-disc"><br>
+        <li>Accompagnement quotidien avec une disponibilité de 12h/24H, 5/7j.</li>
+        <li>Programme sur mesure adapté à vos objectifs et besoins.</li>
+        <li>Suivi et ajustements continus pour maximiser vos résultats.</li>
+        <li>Motivation constante pour vous aider à rester sur la bonne voie.</li>
+      </ul>
+     <p class="pt-4">Le coaching personnalisé vous aide à progresser plus rapidement et qualitativement.</p>
+    `,
+  },
+  {
+    question: "Une fois la formule achetée, que se passe-t-il ?",
+    answer: `
+      Je prends immédiatement contact avec vous pour recueillir vos informations (objectifs, mensurations, photos, etc.) afin de créer votre programme personnalisé. 
+      Dès que vous m'avez renvoyé ces documents, je vous enverrai votre programme d'entraînement et de nutrition pour débuter votre coaching.
+    `,
+  },
+  {
+    question:
+      "En combien de temps vais-je recevoir mon coaching personnalisé ?",
+    answer: `
+      Après réception de vos documents, je crée votre programme immédiatement. Vous le recevrez quelques jours avant le début du coaching pour que vous puissiez vous organiser et discuter de possibles ajustements.
+    `,
+  },
+  {
+    question: "Les bilans hebdomadaires consistent en quoi ?",
+    answer: `
+      Les bilans hebdomadaires permettent de suivre votre progression. Nous examinons vos mensurations, poids, performances et ressentis pour ajuster votre programme. 
+      Un bilan mensuel comparera vos progrès et ajustera le coaching si nécessaire.
+    `,
+  },
+  {
+    question:
+      "Puis-je changer d'objectif en plein milieu d'un coaching personnalisé ?",
+    answer: `
+      Oui, vous pouvez changer d'objectif. Nous discuterons ensemble de vos nouvelles aspirations, et je réajusterai votre programme d'entraînement et de nutrition en fonction de vos nouveaux objectifs.
+    `,
+  },
+  {
+    question: "Quelles sont les attentes de Simon envers moi ?",
+    answer: `
+      Simon attend de vous de la motivation, de la régularité et de la discipline. Si vous êtes prêt à vous engager, vous verrez des changements significatifs dans votre physique et vos résultats.
+    `,
+  },
+];
+export const faqProgramData = [
+  {
+    question: "Quel programme me correspond le mieux ?",
+    answer: `
+      <p><strong>Le coach Simon a établi 3 niveaux de pratiques :</strong></p><br>
+      
+      <ul class="list-disc list-inside pl-4">
+      <p><strong>Programmes Débutants :</strong></p>
+      <li class="pl-4">
+            Parfait pour ceux qui commencent la musculation ou ont peu d'expérience. Ces programmes vous initient aux mouvements et aux bases essentielles, tout en intégrant la nutrition et la récupération pour une progression sûre et adaptée.
+        </li>
+
+        <p><strong>Programmes Intermédiaires :</strong></p>
+      <li class="pl-4">
+    
+            Conçus pour ceux qui s'entraînent régulièrement et souhaitent aller plus loin. Ces programmes vous permettent de repousser vos limites avec des entraînements adaptés, tout en préservant votre équilibre grâce à des conseils sur la nutrition et la récupération.
+  
+        </li>
+
+        <p><strong>Programmes Avancé(e)s :</strong></p>
+      <li class="pl-4">
+      
+            Pour les pratiquants confirmés avec plus d'un an d'expérience. Ces programmes intensifs, avec un volume d'entraînement élevé, visent à maximiser vos performances physiques et mentales.
+    
+       
+            
+            </li>
+            </ul>
+            <p class="mt-10">
+            <strong >Attention :</strong> Choisir un programme avancé alors que vous débutez peut être contre-productif.
+      <p>
+        Sélectionnez un programme adapté à votre niveau pour progresser efficacement. Laissez le coach Simon vous guider vers vos objectifs en toute sécurité.
+      </p>
+    `,
+  },
+  {
+    question:
+      "Pourquoi y a-t-il 2 types de programmes dans chaque niveau d'expérience ?",
+    answer: `
+      <p>Dans chaque niveau d'expérience, vous trouverez deux types de programmes, spécialement conçus par Simon pour répondre à vos besoins en musculation. Cette diversité a été pensée pour vous offrir une flexibilité optimale.</p>
+      
+      <ul class="list-disc list-inside pl-4 py-4">
+        <li>Chaque programme propose la même fréquence et intensité d'entraînement, mais avec des approches différentes pour s'adapter à vos préférences et contraintes.</li>
+        <li>Vous pouvez personnaliser votre expérience en choisissant le programme qui correspond le mieux à votre style de vie et à vos objectifs.</li>
+      </ul>
+
+      <p>Peu importe votre niveau d'expérience, vous avez la liberté de continuer à évoluer avec un programme qui s'adapte parfaitement à vos besoins et à votre emploi du temps. Faites le choix qui vous correspond et atteignez vos objectifs de manière efficace et motivante.</p>
+    `,
+  },
+  {
+    question: "Y a-t-il un suivi des entraînements et de la nutrition ?",
+    answer: `
+      <p>Nos programmes prêts à l'emploi ne proposent pas de suivi personnalisé. Si vous recherchez un accompagnement sur mesure avec un suivi sportif et nutritionnel adapté à vos besoins, nous vous invitons à découvrir nos offres de <strong>"Coaching Personnalisé"</strong> dans la rubrique dédiée.</p>
+      
+      <ul class="list-disc list-inside pl-4 py-4">
+        <li>Les programmes prêts à l'emploi sont conçus pour être autonomes, avec des directives claires sur l'entraînement, la récupération et la nutrition.</li>
+        <li>Ces programmes vous permettent de progresser à votre propre rythme, tout en vous fournissant des informations complètes pour guider votre pratique.</li>
+      </ul>
+
+      <p>Si vous recherchez un accompagnement plus personnalisé, notre coaching sur mesure vous offre un encadrement complet et adapté à vos objectifs spécifiques.</p>
+      
+      <p>Quelle que soit l'option choisie, notre objectif reste de vous aider à atteindre vos résultats et à vous épanouir dans votre pratique sportive. Si vous avez besoin de conseils supplémentaires, le coach Simon est à votre disposition.</p>
+    `,
+  },
+  {
+    question:
+      "Pourquoi choisir un programme déjà fait plutôt qu'un coaching personnalisé ?",
+    answer: `
+      <p>Choisir un programme déjà fait vous offre une solution efficace et abordable pour atteindre vos objectifs sportifs. Conçus avec soin par le coach Simon, nos programmes répondent aux besoins des différents niveaux de progression.</p>
+      
+      <ul class="list-disc list-inside pl-4 py-4">
+        <li>Idéal pour les budgets restreints, tout en garantissant une progression qualitative et sécurisée.</li>
+        <li>Approche structurée et éprouvée, adaptée à votre niveau pour maximiser vos performances.</li>
+        <li>Gain de temps grâce à des directives claires et un contenu complet pour guider votre pratique.</li>
+      </ul>
+      
+      <p>Le coach Simon reste disponible pour répondre à vos questions et vous assurer une expérience enrichissante et motivante. Faites le choix de l'excellence, même avec un budget maîtrisé, en optant pour nos programmes de qualité.</p>
+    `,
+  },
+  {
+    question:
+      "Comment et quand est-ce que je reçois mon programme, après l'achat ?",
+    answer: `
+      <p>Vous recevez immédiatement, après l'achat, le PDF de votre programme sur l'adresse mail que vous avez renseignée.</p>
+      
+      <ul class="list-disc list-inside pl-4 pt-4">
+        <li>Vérifiez attentivement votre adresse mail lors de l'achat pour éviter toute erreur.</li>
+        <li>En cas de problème, contactez-nous pour obtenir de l'aide.</li>
+      </ul>
+    `,
+  },
+  {
+    question: "Combien de temps vais-je pouvoir utiliser mon programme ?",
+    answer: `
+      <p>Il n'y a pas de durée déterminée pour utiliser votre programme. Ces programmes ont été conçus pour être évolutifs et vous accompagner sur le long terme.</p>
+      
+      <ul class="list-disc list-inside pl-4 py-4">
+        <li>Des explications détaillées sur la nutrition, l'entraînement et la récupération vous permettent d'ajuster le programme selon vos besoins.</li>
+        <li>Ces principes vous aident à progresser semaine après semaine, mois après mois, à votre propre rythme.</li>
+      </ul>
+      
+      <p>Votre programme est un outil durable pour vous permettre de continuer à progresser sur le long terme.</p>
+    `,
+  },
+  {
+    question: "Combien de temps le programme m'est-il accessible ?",
+    answer: `
+      <p>Une fois acheté, le programme est à vous, à vie. Vous pourrez le consulter à tout moment, selon vos besoins.</p>
+      
+      <p><strong>ATTENTION :</strong> Il est strictement interdit de partager ce document. Toute violation entraînera des sanctions judiciaires, conformément à notre politique d'utilisation.</p>
+    `,
+  },
+];
---
+++ my-app/app/data/feedbackData.tsx
@@ -0,0 +1,65 @@
+export const feedBack = [
+  {
+    firstName: "Olivia",
+    photo: "/images/feedback/olivia.png",
+    feedback:
+      "Vrai suivi personnalisé avec un coach attentif et impliqué. Résultat rapide et client satisfait.",
+  },
+  {
+    firstName: "Caroline",
+    photo: "/images/feedback/caroline.png",
+    feedback:
+      "Simon, c’est une rencontre marquante. À l’écoute et motivant, il m’a transformé physiquement et moralement. Merci pour ton professionnalisme et tes précieux conseils !",
+  },
+  {
+    firstName: "Alex",
+    photo: "/images/feedback/tete6.png",
+    feedback: "Impeccable pour ton travail, vraiment professionnel.",
+  },
+  {
+    firstName: "Dylan",
+    photo: "/images/feedback/dylan.png",
+    feedback:
+      "Simon m’a aidé à dépasser une période de stagnation et à progresser. Aujourd’hui, je suis autonome grâce à lui.",
+  },
+  {
+    firstName: "Evan",
+    photo: "/images/feedback/evan.png",
+    feedback:
+      "Coach sympa et motivant, Simon m’a aidé à progresser rapidement grâce à ses conseils en entraînement et nutrition. Merci à lui !",
+  },
+  {
+    firstName: "Adrien",
+    photo: "/images/feedback/adrian.png",
+    feedback:
+      "Simon m’a accompagné dans ma transformation. J’ai perdu plus de 40 kg, retrouvé confiance en moi et une vision sereine de l’avenir. Merci Simon !",
+  },
+  // {
+  //   firstName: "Sophie",
+  //   lastName: "Laurent",
+  //   photo: "/images/tete7.png",
+  //   feedback:
+  //     "Tout est super bien pensé. Même en ayant peu de temps, on peut progresser efficacement.",
+  // },
+  // {
+  //   firstName: "Julien",
+  //   lastName: "Simon",
+  //   photo: "/images/tete4.png",
+  //   feedback:
+  //     "Je cherchais quelque chose de simple et efficace, et c’est exactement ce que j’ai trouvé ici.",
+  // },
+  // {
+  //   firstName: "Camille",
+  //   lastName: "Rousseau",
+  //   photo: "/images/tete3.png",
+  //   feedback:
+  //     "L’approche est différente de tout ce que j’ai testé avant. Enfin quelque chose qui marche vraiment !",
+  // },
+  // {
+  //   firstName: "Maxime",
+  //   lastName: "Lemoine",
+  //   photo: "/images/tete2.png",
+  //   feedback:
+  //     "Les résultats sont bluffants. Je me sens bien mieux dans mon corps et dans ma tête.",
+  // },
+];
