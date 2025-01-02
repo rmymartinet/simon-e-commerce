a0fa3335b082bafcc07a6f80730b886c007f4ae6
rmymartinet <remymartinet.contact@gmail.com>
2025-01-02 10:16:22 +0100

    Replace the AvailableOffers components with AvailableOfferContainer in the homepage

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -4,8 +4,6 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import BeforeAfterPhoto from "./_components/BeforeAfterPhoto";
 import Header from "./_components/Header/Header";
-import AvailableOffers from "./_components/AvailableOffers";
-import { coachingIncludes9Mounth, programIncludes } from "./data/cardPrice";
 import Youtube from "./_components/program/Youtube";
 import NutritionGrid from "./_components/coaching/NutritionGrid";
 import ProgramOverview from "./_components/program/ProgramOverview";
@@ -15,6 +13,7 @@ import AppDetails from "./_components/coaching/AppDetails";
 import CoachingNutrition from "./_components/coaching/CoachingNutrition";
 import AppOverview from "./_components/coaching/AppOverview";
 import EvolvingProgram from "./_components/program/EvolvingProgram";
+import AvailableOfferContainer from "./_components/AvailableOfferContainer";
 
 gsap.registerPlugin(useGSAP);
 
@@ -22,20 +21,7 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center">
       <Header />
-      <div className="mt-[20vh] flex flex-col gap-10 lg:grid lg:grid-cols-2">
-        <AvailableOffers
-          title="Programmes"
-          follow="Sans suivi"
-          subtitle="Adapté à votre niveau"
-          features={programIncludes}
-        />
-        <AvailableOffers
-          title="Coaching"
-          follow="Avec suivi"
-          subtitle="Suivi sur mesure"
-          features={coachingIncludes9Mounth}
-        />
-      </div>
+      <AvailableOfferContainer />
       <ProgramOverview />
       <EvolvingProgram />
       <ProgramNutrition />
@@ -46,20 +32,7 @@ export default function Home() {
       <CoachingNutrition />
       <NutritionGrid />
       <BeforeAfterPhoto />
-      <div className="relative mt-[20vh] flex flex-col gap-10 lg:grid lg:grid-cols-2">
-        <AvailableOffers
-          title="Programmes"
-          follow="Sans suivi"
-          subtitle="Adapté à votre niveau"
-          features={programIncludes}
-        />
-        <AvailableOffers
-          title="Coaching"
-          follow="Avec suivi"
-          subtitle="Suivi sur mesure"
-          features={coachingIncludes9Mounth}
-        />
-      </div>
+      <AvailableOfferContainer />
     </section>
   );
 }
