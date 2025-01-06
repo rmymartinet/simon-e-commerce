f56c359f0bbd46b1057bb3f86c81c81f9673884b
rmymartinet <remymartinet.contact@gmail.com>
2025-01-06 23:02:08 +0100

    Added MobileCart: displays the number of items in the cart, optimized for mobile for better clarity

---
+++ my-app/app/_components/Cart/MobileCart.tsx
@@ -0,0 +1,26 @@
+import { useCart } from "@/app/context/CartContext";
+import { FaCartShopping } from "react-icons/fa6";
+
+//  right-5 top-10
+const MobileCart = ({
+  position,
+  xPostion,
+  yPostion,
+}: {
+  position?: string;
+  xPostion?: string;
+  yPostion?: string;
+}) => {
+  const { cart } = useCart();
+
+  return (
+    <div className={`${position} ${xPostion} ${yPostion} flex w-max flex-col`}>
+      <div className="absolute -bottom-4 -right-4 grid h-6 w-6 place-content-center rounded-full bg-violet-400 text-sm text-white">
+        {cart.length}
+      </div>
+      <FaCartShopping className="text-xl" color="white" />
+    </div>
+  );
+};
+
+export default MobileCart;
