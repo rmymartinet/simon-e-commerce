55fec9a54a02977f6621c223ba7b51f3e47d6ea1
rmymartinet <remymartinet.contact@gmail.com>
2025-01-06 23:03:19 +0100

    Added mobile navigation and MobileCart: integrated MobileNav and MobileCart for an improved user experience on smaller screens

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -8,6 +8,9 @@ import { usePathname } from "next/navigation";
 import "./globals.css";
 import { CartProvider } from "./context/CartContext";
 import { CheckoutProvider } from "./context/CheckoutContext";
+import useWindowWidth from "@/hooks/useWindowWidth";
+import MobileNav from "./_components/Nav/MobileNav";
+import MobileCart from "./_components/Cart/MobileCart";
 
 export default function RootLayout({
   children,
@@ -28,13 +31,22 @@ export default function RootLayout({
     pathname !== "/success" &&
     pathname !== "/cancel";
 
+  const { width } = useWindowWidth();
+
   return (
     <html lang="en">
       <body id="main-container" className="antialiased">
         <CartProvider>
           <CheckoutProvider>
             <AnimationProvider>
-              {isDiplayNavBar && <NavBar />}
+              {isDiplayNavBar && width < 1024 ? <MobileNav /> : <NavBar />}
+              {isDiplayNavBar && width < 1024 && (
+                <MobileCart
+                  position="fixed"
+                  xPostion="right-5"
+                  yPostion="top-10"
+                />
+              )}
               {children}
               {pathname !== "/studio" && <Footer />}
             </AnimationProvider>
