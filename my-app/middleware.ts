369b62a26682a4e140d6b290ee7a021b1c2d97a5
rmymartinet <remymartinet.contact@gmail.com>
2025-01-19 13:27:05 +0100

    Correction du chemin d'importation de la fonction decrypt dans le middleware

--- my-app/middleware.ts
+++ my-app/middleware.ts
@@ -1,6 +1,6 @@
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-import { decrypt } from "./app/lib/session";
+import { decrypt } from "./app/_lib/session";
 
 const protectedRoutes = ["/dashboard"];
 const publicRoutes = ["/login"];
