e6ccd81c9b241c2c1862abbe8b73e6d4f08e1da4
rmymartinet <remymartinet.contact@gmail.com>
2025-01-21 11:00:37 +0100

    Removal of the CORS headers configuration in `next.config.ts`.

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,34 +1,5 @@
 import type { NextConfig } from "next";
 
-const headers = [
-  "Accept",
-  "Accept-Version",
-  "Content-Length",
-  "Content-MD5",
-  "Content-Type",
-  "Date",
-  "X-Api-Version",
-  "X-CSRF-Token",
-  "X-Requested-With",
-];
-
-module.exports = {
-  reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/api/(.*)",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {  key: 'Access-Control-Allow-Origin', value: `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}` },
-          { key: "Access-Control-Allow-Methods", value: "GET,POST" },
-          { key: "Access-Control-Allow-Headers", value: headers.join(", ") }
-          ]
-      }
-    ];
-  }
-};
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
