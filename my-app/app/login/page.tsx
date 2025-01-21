e63e1863a6f82e998ca0b11cb87034310205d888
rmymartinet <remymartinet.contact@gmail.com>
2025-01-21 11:02:47 +0100

    Refactor login page to implement Google and GitHub sign-in forms

--- my-app/app/login/page.tsx
+++ my-app/app/login/page.tsx
@@ -1,5 +1,25 @@
-import LoginFormContainer from "./LoginFormContainer";
+import { signIn } from "@/auth";
 
-export default function Login() {
-  return <LoginFormContainer />;
+export default function SignIn() {
+  return (
+    <div className="grid h-screen place-content-center">
+      <form
+        action={async () => {
+          "use server";
+          await signIn("google", { redirectTo: "/dashboard" });
+        }}
+      >
+        <button type="submit">Signin with Google</button>
+      </form>
+
+      <form
+        action={async () => {
+          "use server";
+          await signIn("github", { redirectTo: "/dashboard" });
+        }}
+      >
+        <button type="submit">Signin with Github</button>
+      </form>
+    </div>
+  );
 }
