651abe169b60973da2882323afd31284d34e16af
rmymartinet <remymartinet.contact@gmail.com>
2025-01-25 01:33:35 +0100

    Refactor Youtube component: remove unused refs and GSAP animations

--- my-app/app/_components/program/Youtube.tsx
+++ my-app/app/_components/program/Youtube.tsx
@@ -9,48 +9,11 @@ import BackgroundYoutubeImg from "./BackgroundYoutubeImg";
 gsap.registerPlugin(ScrollTrigger, useGSAP);
 
 const Youtube = () => {
-  const youtubeLogoRef = useRef<HTMLDivElement>(null);
   const youtubeTitleRef = useRef<HTMLDivElement>(null);
-  const bgBlackRef = useRef<HTMLDivElement>(null);
   const iphoneRef = useRef<HTMLDivElement>(null);
   const imagesRefs = useRef<(HTMLDivElement | null)[]>([]);
 
   useGSAP(() => {
-    gsap.fromTo(
-      youtubeLogoRef.current,
-      {
-        scale: 4,
-        x: 200,
-
-        duration: 1,
-      },
-      {
-        scrollTrigger: {
-          trigger: youtubeLogoRef.current,
-          start: "top 80%",
-          end: "bottom 80%",
-          scrub: 1,
-          once: false,
-        },
-        x: 0,
-        scale: 1,
-        duration: 1,
-      },
-    );
-
-    gsap.to(bgBlackRef.current, {
-      scrollTrigger: {
-        trigger: bgBlackRef.current,
-        start: "top bottom",
-        end: "bottom 80%",
-        scrub: 1,
-        once: false,
-      },
-      width: "0%",
-      delay: 1,
-      duration: 1,
-    });
-
     imagesRefs.current.forEach((img) => {
       gsap.to(img, {
         scrollTrigger: {
@@ -70,17 +33,13 @@ const Youtube = () => {
     <div className="mt-[20vh] flex flex-col items-center">
       <div className="mb-20 flex flex-col items-center">
         <div className="relative flex items-center gap-4">
-          <div ref={youtubeLogoRef} className="relative z-[9999]">
+          <div  className="relative">
             <FaYoutube color="red" size={30} />
             <div className="absolute left-1/2 top-1/2 -z-10 h-3 w-3 -translate-x-1/2 -translate-y-1/2 bg-white"></div>
           </div>
           <h1 ref={youtubeTitleRef} className="mr-10 text-3xl font-bold">
             Youtube
           </h1>
-          <div
-            ref={bgBlackRef}
-            className="absolute z-30 h-full w-full bg-[#0b0d14]"
-          ></div>
         </div>
         <p className="text-pretty text-center text-lg font-medium text-subtle">
           Tous vos exercices en vidéo, accessibles uniquement aux membres
