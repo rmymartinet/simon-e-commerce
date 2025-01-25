386fa51cc2a8c20a56cdcd84160496e7c6b5c2f5
rmymartinet <remymartinet.contact@gmail.com>
2025-01-25 13:45:58 +0100

    Updating the authentication redirection to point to the sign-in page (`/sign-in`) instead of `/login`.

--- my-app/middlewate.ts
+++ my-app/middlewate.ts
@@ -1,8 +1,8 @@
 import { auth } from "@/app/_lib/auth";
 
 export default auth((req) => {
-  if (!req.auth && req.nextUrl.pathname !== "/login") {
-    const newUrl = new URL("/login", req.nextUrl.origin);
+  if (!req.auth && req.nextUrl.pathname !== "/sign-in") {
+    const newUrl = new URL("/sign-in", req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
 });
