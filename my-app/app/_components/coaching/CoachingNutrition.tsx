a4de65ab1febd079c9baf660a83b49535a516fea
rmymartinet <remymartinet.contact@gmail.com>
2025-01-27 16:26:13 +0100

    Refactor CoachingNutrition component to replace ColorShadowButton with reusable Button component, update layout for improved spacing, and enhance text consistency.

--- my-app/app/_components/coaching/CoachingNutrition.tsx
+++ my-app/app/_components/coaching/CoachingNutrition.tsx
@@ -1,12 +1,12 @@
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 import gsap from "gsap";
-import ColorShadowButton from "../ColorShadowButton";
 import { coachingNutritionData } from "@/app/data/coachingNutritionData";
 import Accordion from "../Accordion";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Image from "next/image";
 import useWindowWidth from "@/hooks/useWindowWidth";
+import Button from "../Button";
 
 gsap.registerPlugin(ScrollTrigger, useGSAP);
 
@@ -32,34 +32,36 @@ const CoachingNutrition = () => {
   const { width } = useWindowWidth();
 
   return (
-    <div className="mt-[20vh] flex w-full flex-col gap-20 bg-white px-4 text-black">
-      <div className="mb-10 flex flex-col md:mb-40">
+    <div className="mt-[20vh] flex w-full flex-col gap-20 px-4">
+      <div className="mb-10 flex flex-col gap-20 md:mb-20">
         <p className="max-w-5xl text-pretty break-words text-3xl lg:text-7xl">
           Un suivi nutritionnels personnalisé adpaté à vos besoins
         </p>
-        <ColorShadowButton title="Voir les offres" color="#f690ff" />
+        <div className="self-center">
+          <Button href="/pricing" />
+        </div>
       </div>
       <div className="flex flex-col gap-20 lg:grid lg:grid-cols-2">
         {width > 1024 && (
           <div className="border-2 border-red-400">
             <p>( A effectuer )</p>
-            <span className="text-light max-w-lg lg:text-[20vw]">01</span>
+            <span className="max-w-lg text-light lg:text-[20vw]">01</span>
           </div>
         )}
         <div className="flex flex-col gap-8">
           <div className="flex justify-between">
-            <p className="text-xl">Mise en place</p>
+            <p className="text-xl">Processus</p>
             <p className="text-xl">Mise en place</p>
           </div>
           <div>
             {coachingNutritionData.map((item, index) => (
               <div key={index}>
-                <div className="bg-light h-[2px]">
+                <div className="h-[2px] bg-[#27262a]">
                   <div
                     ref={(el) => {
                       lineRefs.current[index] = el;
                     }}
-                    className="h-full w-0 bg-[#27262a]"
+                    className="h-full w-0 bg-muted"
                   ></div>
                 </div>
                 <Accordion
@@ -76,7 +78,7 @@ const CoachingNutrition = () => {
         {width > 1024 && (
           <div>
             <p className="text-3xl">( Communiquer en tant reel )</p>
-            <span className="text-light max-w-lg lg:text-[20vw]">02</span>
+            <span className="max-w-lg text-light lg:text-[20vw]">02</span>
           </div>
         )}
         <div>
