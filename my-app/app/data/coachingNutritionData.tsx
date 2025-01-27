287a3ceb0a089945075f88cb533a47609ec747be
rmymartinet <remymartinet.contact@gmail.com>
2025-01-27 16:28:04 +0100

    Update the descriptions in coachingNutritionData for better clarity and personalization of the services offered

--- my-app/app/data/coachingNutritionData.tsx
+++ my-app/app/data/coachingNutritionData.tsx
@@ -2,24 +2,24 @@ export const coachingNutritionData = [
   {
     title: "Une Analyse complète",
     description:
-      "Je vous aide à déterminer vos besoins nutritionnels et à identifier les changements à apporter à votre alimentation.",
+      "Je réalise une analyse approfondie de vos besoins nutritionnels en tenant compte de votre mode de vie, de vos objectifs et de vos préférences. Ensemble, nous identifions les points à améliorer dans votre alimentation pour maximiser vos résultats.",
   },
 
   {
     title: "Création de votre plan nutritionnel",
     description:
-      "Je vous propose un plan alimentaire adapté à vos objectifs et à vos préférences alimentaires.",
+      "Je conçois un plan alimentaire personnalisé qui répond à vos objectifs spécifiques, qu'il s'agisse de perte de poids, de prise de masse musculaire ou simplement d'une alimentation plus équilibrée. Ce plan est adapté à vos goûts et à vos contraintes pour garantir qu'il s'intègre facilement à votre quotidien.",
   },
 
   {
     title: "Des Recettes adaptées",
     description:
-      "Je vous propose des recettes saines et équilibrées, adaptées à vos goûts et à vos contraintes.",
+      "Je vous fournis des recettes variées, saines et équilibrées, adaptées à vos goûts, votre budget et vos besoins nutritionnels. Chaque recette est conçue pour être facile à préparer et pour soutenir vos progrès.",
   },
 
   {
-    title: "Ajustements",
+    title: "Ajustements et suivi",
     description:
-      "Je vous accompagne dans la durée pour vous aider à atteindre vos objectifs et à maintenir vos résultats.",
+      "Je vous accompagne tout au long de votre parcours en ajustant votre plan nutritionnel au fil du temps. Grâce à un suivi régulier, nous nous assurons que vous restez sur la bonne voie pour atteindre vos objectifs et maintenir vos résultats sur le long terme.",
   },
 ];
