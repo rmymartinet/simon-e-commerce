89487b28048eb0cba5be4ecb6d62dfb3eda5146c
rmymartinet <remymartinet.contact@gmail.com>
2025-01-28 23:57:30 +0100

    Added the ability to customize the button title in the Button component.

--- my-app/app/_components/Button.tsx
+++ my-app/app/_components/Button.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
 
-function Button({ href }: { href: string }) {
+function Button({
+  title = "Découvrir",
+  href,
+}: {
+  title?: string;
+  href: string;
+}) {
   return (
     <Link
       href={href}
       className="h-max w-max rounded-full border border-foreground px-6 py-2 text-white transition-all duration-200 ease-linear hover:border-violet-400 hover:bg-violet-400"
     >
-      <button>Découvrir</button>
+      <button>{title}</button>
     </Link>
   );
 }
