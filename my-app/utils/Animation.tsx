9e0339492d43bc2739488e34f207aff41f71956b
rmymartinet <remymartinet.contact@gmail.com>
2025-01-28 16:40:24 +0100

    Added an AnimationContext to manage animation state and updated animation functions to improve page transition handling.

--- my-app/utils/Animation.tsx
+++ my-app/utils/Animation.tsx
@@ -1,5 +1,7 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -20,4 +22,47 @@ export const revealRotateEl = (ref: React.RefObject<HTMLDivElement>) => {
       start: "top bottom",
     },
   });
+};
+
+gsap.registerPlugin(useGSAP);
+
+export const animatePageIn = () => {
+  const sentence = document.getElementById("sentence");
+
+  if (sentence) {
+    gsap.set(sentence, {
+      yPercent: 0,
+    });
+
+    gsap.to(sentence, {
+      delay: 0.5,
+      yPercent: 200,
+      duration: 0.7,
+      ease: "power3.inOut",
+    });
+  }
+};
+
+export const animatePageOut = (
+  href: string,
+  router: AppRouterInstance,
+  onComplete: () => void,
+) => {
+  const sentence = document.getElementById("sentence");
+
+  if (sentence) {
+    gsap.set(sentence, {
+      yPercent: -200,
+    });
+
+    gsap.to(sentence, {
+      yPercent: 0,
+      duration: 0.7,
+      ease: "power3.inOut",
+      onComplete: () => {
+        router.push(href);
+        onComplete();
+      },
+    });
+  }
 };
