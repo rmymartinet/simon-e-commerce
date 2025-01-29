7da4fbb14cf92daf4913b22545d8f12715a9a184
rmymartinet <remymartinet.contact@gmail.com>
2025-01-29 15:26:29 +0100

    Improved the presentation of the `FeedBackContainer` component: adjusted the text size for better readability.

--- my-app/app/_components/FeedBack/FeedBackContainer.tsx
+++ my-app/app/_components/FeedBack/FeedBackContainer.tsx
@@ -5,7 +5,7 @@ const FeedBackContainer = () => {
     <section className="grid grid-rows-feedBackContainer justify-items-center gap-10">
       <div className="flex flex-col items-start lg:items-center">
         <h1 className="text-3xl md:text-4xl lg:text-7xl">Nos Résulats</h1>
-        <p className="font-medium text-muted md:text-3xl">
+        <p className="max-w-5xl font-medium text-muted md:text-xl">
           Découvrez ce que nos clients disent de notre équipe et de nos services
         </p>
       </div>
