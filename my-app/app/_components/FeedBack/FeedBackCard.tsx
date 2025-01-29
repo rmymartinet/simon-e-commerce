b6816da4b82a3c7d328ca0bc8abfe3deffddb4c2
rmymartinet <remymartinet.contact@gmail.com>
2025-01-29 01:23:37 +0100

    Increased the height of the feedback card for better visual presentation.

--- my-app/app/_components/FeedBack/FeedBackCard.tsx
+++ my-app/app/_components/FeedBack/FeedBackCard.tsx
@@ -4,7 +4,7 @@ import { RiStarSFill } from "react-icons/ri";
 
 const FeedBackCard = ({ firstName, imgUrl, text }: FeedBackCardProps) => {
   return (
-    <div className="card grid h-[35vh] grid-rows-feedBackCard justify-between rounded-2xl border border-[#424242] p-6">
+    <div className="card grid h-[40vh] grid-rows-feedBackCard justify-between rounded-2xl border border-[#424242] p-6">
       <div className="mb-8 flex gap-2">
         {Array(5)
           .fill(null)
