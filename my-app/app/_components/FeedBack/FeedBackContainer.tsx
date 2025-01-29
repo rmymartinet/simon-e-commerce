7b178cf42bc71f9de29f9dd3139609d6bc281d4d
rmymartinet <remymartinet.contact@gmail.com>
2025-01-29 14:55:17 +0100

    Improved the presentation in the `FeedBackContainer` component: adjusted text styles for better readability and responsiveness.

--- my-app/app/_components/FeedBack/FeedBackContainer.tsx
+++ my-app/app/_components/FeedBack/FeedBackContainer.tsx
@@ -3,11 +3,9 @@ import FeedBackCarouselle from "./FeedbackCarouselle";
 const FeedBackContainer = () => {
   return (
     <section className="grid grid-rows-feedBackContainer justify-items-center gap-10">
-      <div className="flex flex-col items-center">
-        <h1 className="text-center text-6xl font-bold md:text-7xl">
-          Nos Résulats
-        </h1>
-        <p className="text-center text-lg font-semibold text-textOpacity">
+      <div className="flex flex-col items-start lg:items-center">
+        <h1 className="text-3xl md:text-4xl lg:text-7xl">Nos Résulats</h1>
+        <p className="font-medium text-muted md:text-3xl">
           Découvrez ce que nos clients disent de notre équipe et de nos services
         </p>
       </div>
