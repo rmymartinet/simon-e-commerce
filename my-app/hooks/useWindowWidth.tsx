b13d17879c9183ae584a603479710b810e8816cf
rmymartinet <remymartinet.contact@gmail.com>
2025-01-29 21:40:15 +0100

    Refactor useWindowWidth hook to use useLayoutEffect for improved performance on window resize

--- my-app/hooks/useWindowWidth.tsx
+++ my-app/hooks/useWindowWidth.tsx
@@ -1,22 +1,22 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 
 export default function useWindowWidth() {
   const [width, setWidth] = useState(0);
-  const [isMounted, setIsMounted] = useState(false); // Ajout d'un état pour le rendu
+  const [isMounted, setIsMounted] = useState(false);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (typeof window !== "undefined") {
       const handleResize = () => setWidth(window.innerWidth);
 
-      handleResize(); // Met à jour la largeur initialement
-      setIsMounted(true); // Indique que le composant est monté
+      handleResize();
+      setIsMounted(true);
 
       window.addEventListener("resize", handleResize);
       return () => window.removeEventListener("resize", handleResize);
     }
   }, []);
 
-  return { width, isMounted }; // Renvoie également isMounted
+  return { width, isMounted };
 }
