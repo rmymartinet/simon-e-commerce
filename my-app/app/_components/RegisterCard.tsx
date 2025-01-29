ffadfecdf6f34cbbda11c97345b93b3b15ebbf62
rmymartinet <remymartinet.contact@gmail.com>
2025-01-29 15:39:00 +0100

    Adjusted the layout of the authentication choice page and the `RegisterCard` component to improve spacing and responsiveness.

--- my-app/app/_components/RegisterCard.tsx
+++ my-app/app/_components/RegisterCard.tsx
@@ -1,7 +1,7 @@
 const RegisterCard = ({ children }: { children: React.ReactNode }) => {
   return (
     <section className="grid w-full place-content-center">
-      <div className="flex w-[400px] flex-col items-center gap-10 rounded-lg bg-white p-8">
+      <div className="flex w-full flex-col items-center gap-10 rounded-lg bg-white p-8 md:w-[400px]">
         {children}
       </div>
     </section>
