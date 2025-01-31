c36fddc2705ef340256931f590caef67d5dbcfba
rmymartinet <remymartinet.contact@gmail.com>
2025-01-31 18:12:46 +0100

    Removed the `isMounted` state in the `useWindowWidth` hook and simplified the function's return.

--- my-app/hooks/useWindowWidth.tsx
+++ my-app/hooks/useWindowWidth.tsx
@@ -4,19 +4,17 @@ import { useLayoutEffect, useState } from "react";
 
 export default function useWindowWidth() {
   const [width, setWidth] = useState(0);
-  const [isMounted, setIsMounted] = useState(false);
 
   useLayoutEffect(() => {
     if (typeof window !== "undefined") {
       const handleResize = () => setWidth(window.innerWidth);
 
       handleResize();
-      setIsMounted(true);
 
       window.addEventListener("resize", handleResize);
       return () => window.removeEventListener("resize", handleResize);
     }
   }, []);
 
-  return { width, isMounted };
+  return { width };
 }
