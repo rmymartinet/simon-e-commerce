8b82db0ae1c253d7a2e67a5b70483c013a9d78b2
rmymartinet <remymartinet.contact@gmail.com>
2025-02-01 13:26:13 +0100

    Refactor sendResetPasswordEmail function to use callback pattern for sending emails and improve error handling

--- my-app/app/_lib/sendRestPasswordEmail.ts
+++ my-app/app/_lib/sendRestPasswordEmail.ts
@@ -5,20 +5,13 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
-  connectionTimeout: 20000,
-  socketTimeout: 20000,
-  logger: true,
-  debug: true,
 });
 
-export async function sendResetPasswordEmail(email: string, token: string) {
+async function sendResetPasswordEmail(email: string, token: string) {
   const resetUrl = `https://www.smartinet-coaching.com/reset-password?token=${token}`;
 
   const mailOptions = {
@@ -29,14 +22,22 @@ export async function sendResetPasswordEmail(email: string, token: string) {
     html: `<p>Vous avez demandé à réinitialiser votre mot de passe. Cliquez sur le lien suivant pour réinitialiser votre mot de passe :</p><p><a href="${resetUrl}">Réinitialiser le mot de passe</a></p>`,
   };
 
-  try {
-    await transporter.sendMail(mailOptions);
-    console.log("Email de réinitialisation envoyé à :", email);
-  } catch (error) {
-    console.error(
-      "Erreur lors de l'envoi de l'email de réinitialisation :",
-      error,
-    );
-    throw new Error("Erreur lors de l'envoi de l'email de réinitialisation.");
-  }
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(
+          "Erreur lors de l'envoi de l'email de réinitialisation :",
+          error,
+        );
+        reject(
+          new Error("Erreur lors de l'envoi de l'email de réinitialisation."),
+        );
+      } else {
+        console.log("Email de réinitialisation envoyé à :", email);
+        resolve(info);
+      }
+    });
+  });
 }
+
+export { sendResetPasswordEmail };
