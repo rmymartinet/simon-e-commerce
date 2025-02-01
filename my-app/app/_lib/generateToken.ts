ae6a93caa4c2f7212fe31f108f280ddedc0af1c8
rmymartinet <remymartinet.contact@gmail.com>
2025-02-01 11:48:29 +0100

    Added password reset token generation and email sending functionality, updated forgotten password API.

---
+++ my-app/app/_lib/generateToken.ts
@@ -0,0 +1,18 @@
+import { prisma } from "@/app/_lib/prisma";
+import crypto from "crypto";
+import { sendResetPasswordEmail } from "@/app/_lib/sendRestPasswordEmail";
+
+export async function generateResetToken(email: string) {
+  const token = crypto.randomBytes(32).toString("hex");
+  const expires = new Date(Date.now() + 3600000); // 1 heure
+
+  await prisma.user.update({
+    where: { email },
+    data: {
+      resetPasswordToken: token,
+      resetPasswordExpires: expires,
+    },
+  });
+
+  await sendResetPasswordEmail(email, token);
+}
--- my-app/app/api/auth/password/forgot-password/route.ts
+++ my-app/app/api/auth/password/forgot-password/route.ts
@@ -1,81 +1,36 @@
-import { hashPassword } from "@/app/_lib/hashPassword";
+import { generateResetToken } from "@/app/_lib/generateToken";
 import { prisma } from "@/app/_lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const { token, password } = await req.json();
+    const { email } = await req.json();
 
-    const user = await prisma.user.findFirst({
+    const user = await prisma.user.findUnique({
       where: {
-        resetPasswordToken: token,
-        resetPasswordExpires: {
-          gt: new Date(),
-        },
+        email: email,
       },
     });
 
     if (!user) {
-      return NextResponse.json(
-        { message: "Token invalide ou expiré" },
-        { status: 400 },
-      );
+      console.log("Invalid or expired token");
+      return NextResponse.json({ message: "Email invalide" }, { status: 400 });
     }
 
-    const hashedPassword = await hashPassword(password);
-    await prisma.user.update({
-      where: { id: user.id },
-      data: {
-        password: hashedPassword,
-        resetPasswordToken: null,
-        resetPasswordExpires: null,
-      },
-    });
+    await generateResetToken(email);
 
     return NextResponse.json(
-      { message: "Mot de passe réinitialisé avec succès" },
+      {
+        message: "Mail pour réinitialisation de mot de passe envoyé",
+        success: true,
+      },
       { status: 200 },
     );
   } catch (error) {
-    console.error(error);
+    console.error("Error processing request:", error);
     return NextResponse.json(
       { message: "Erreur lors de la réinitialisation du mot de passe" },
       { status: 500 },
     );
   }
-}
-
-export async function GET(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
-  const token = searchParams.get("token");
-
-  const user = await prisma.user.findFirst({
-    where: {
-      resetPasswordToken: token,
-      resetPasswordExpires: {
-        gt: new Date(),
-      },
-    },
-  });
-
-  if (!user) {
-    return NextResponse.redirect("/404");
-  }
-
-  return NextResponse.json({ message: "Token valide" });
-}
-
-export async function generateResetToken(email) {
-  const token = crypto.randomBytes(32).toString("hex");
-  const expires = new Date(Date.now() + 3600000);
-
-  await prisma.user.update({
-    where: { email },
-    data: {
-      resetPasswordToken: token,
-      resetPasswordExpires: expires,
-    },
-  });
-
-  await sendResetPasswordEmail(email, token);
 }
