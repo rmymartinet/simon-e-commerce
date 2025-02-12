b8219e38482fef0c41bf8dd02a8d95d19e599dd9
rmymartinet <remymartinet.contact@gmail.com>
2025-02-12 11:23:44 +0100

    Added Google Analytics with scripts for user tracking to the main layout.

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -8,6 +8,7 @@ import Footer from "./_components/Footer/Footer";
 import "./globals.css";
 import { LayoutTransition } from "./_components/pageTransitions/LayoutTransition";
 import LocomotiveScrollWrapper from "./_components/LocomotiveScrollWrapper";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -20,6 +21,21 @@ export default function RootLayout({
         <CheckoutProvider>
           <AnimationProvider>
             <html lang="en">
+              <head>
+                <Script
+                  async
+                  src="https://www.googletagmanager.com/gtag/js?id=G-NH0VPNGP9L"
+                ></Script>
+                <Script id="google-analytic">
+                  {` 
+                  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NH0VPNGP9L');
+  `}
+                </Script>
+              </head>
               <body id="main-container" className="antialiased">
                 <ConditionalNav />
                 <LayoutTransition>{children}</LayoutTransition>
