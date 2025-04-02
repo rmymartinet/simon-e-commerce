136aef8aad709dfafe57bdd2a6d0fa7b988a3868
rmymartinet <remymartinet.contact@gmail.com>
2025-04-02 17:01:03 +0200

    feat: add utility function for class name merging and integrate TitleComponent in Calorie page for improved layout and readability

--- my-app/app/calories/page.tsx
+++ my-app/app/calories/page.tsx
@@ -4,7 +4,9 @@ import { useEffect, useRef } from "react";
 import FormCalories from "../_components/Calories/FormCalories";
 import { useAnimation } from "../context/AnimationContext";
 import gsap from "gsap";
-import OffersOverview from "../_components/OffersOverview";
+import TitleComponent from "../_components/TitleComponent";
+import AnimatedQuestions from "../_components/AnimatedQuestions";
+import { Button } from "../_components/ui/button";
 
 export default function Calorie() {
   const { isAnimating } = useAnimation();
@@ -18,6 +20,15 @@ export default function Calorie() {
     "Que manger pour maigrir ?",
     "Combien de calories manger ?",
     "Combien de protéines consommer ?",
+    "Quel entraînement pour perdre du gras ?",
+    "Faut-il faire du cardio ou de la musculation ?",
+    "Comment éviter l’effet yoyo ?",
+    "Quels aliments éviter pour sécher ?",
+    "Faut-il manger le soir pour progresser ?",
+    "Puis-je perdre du poids sans sport ?",
+    "Comment suivre mes progrès efficacement ?",
+    "Quelle routine alimentaire adopter ?",
+    "Comment retrouver la motivation ?",
   ];
 
   useEffect(() => {
@@ -42,38 +53,26 @@ export default function Calorie() {
       ref={containerRef}
       className="mt-[25vh] flex min-h-screen flex-col items-center justify-center gap-20 overflow-hidden"
     >
-      <div className="mb-8 flex w-full flex-col flex-wrap items-center justify-center gap-2">
-        <span className="mb-10 text-sm uppercase text-gray-500">Calories</span>
-        <h1 className="text-center text-2xl font-semibold md:text-5xl">
-          Calcul des besoins caloriques journaliers (TDEE).
-        </h1>
-        <p className="text-center">
-          Ce calculateur de macro est un outil unique qui permet de calculer tes
+      <TitleComponent
+        titleIndication="Calories"
+        title="Calcul des besoins caloriques journaliers (TDEE)."
+        subtitle=" Ce calculateur de macro est un outil unique qui permet de calculer tes
           apports caloriques personnalisés en fonction de tes dépenses
-          énergiques et de ton alimentation.
-        </p>
-      </div>
+          énergiques et de ton alimentation."
+      />
+      <Button variant="blackBg">Découvrir nos offres</Button>
       <FormCalories />
-      <div className="flex w-full flex-col px-4">
-        <h1 className="text-3xl md:text-4xl lg:text-7xl">
-          Vous voulez gagner du temps ?
-        </h1>
-        <p className="font-medium text-muted md:text-xl">
-          Vous vous êtes sûrement posé ces questions
-        </p>
-        <div className="mt-10 flex flex-col justify-center gap-4">
-          {questions.map((quesiton, index) => (
-            <div
-              key={index}
-              className="flex w-full items-center gap-10 border-b border-muted py-8"
-            >
-              <p className="text-muted lg:text-3xl">0{index + 1}</p>
-              <p className="lg:text-4xl">{quesiton}</p>{" "}
-            </div>
-          ))}
+      <div className="flex w-full flex-col items-center px-4">
+        <TitleComponent
+          title="Vous voulez gagner du temps ?"
+          subtitle=" Vous vous êtes sûrement posé ces questions"
+          isTextSplitLines={false}
+        />
+        <div className="mt-10">
+          <Button variant="blackBg">Découvrir nos offres</Button>
         </div>
+        <AnimatedQuestions questions={questions} />
       </div>
-      <OffersOverview />
     </div>
   );
 }
