97f2e807eb0b642eb68fe11ab900ec18154a22a0
rmymartinet <remymartinet.contact@gmail.com>
2025-04-08 11:05:34 +0200

    style: update auth client base URL to production server

--- my-app/lib/auth-client.ts
+++ my-app/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /** the base url of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
+  baseURL: "https://www.smartinet-coaching.com",
 });
 
 export const { signIn, signOut, signUp, useSession } = authClient;
