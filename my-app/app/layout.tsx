166d5218ad5f342a2f0f8e698ef82a462be8bcdf
rmymartinet <remymartinet.contact@gmail.com>
2026-01-13 20:43:56 +0100

    adding conditional nav

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import Script from "next/script";
 import Footer from "@/components/Footer/Footer";
 import LocomotiveScrollWrapper from "@/components/LocomotiveScrollWrapper";
+import ConditionalNav from "@/components/Nav/ConditionalNav";
 import Providers from "./providers";
 import "../app/globals.css";
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       </head>
       <body id="main-container" className="antialiased">
         <Providers>
+          <ConditionalNav />
           <main className="pb-20">{children}</main>
         </Providers>
         <Footer />
